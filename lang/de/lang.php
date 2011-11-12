719e6564fce071ea47210a9f169a2d7b065589fe
Christoph Ziehr <info@einsatzleiterwiki.de>
2011-11-12 20:27:40 +0100

    German translation added

---
+++ lang/de/lang.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * German language file for upgrade plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Wiki aktualisieren';
+
+// custom language strings for the plugin
+$lang['dl_from'] = 'Archiv wird von %s heruntergeladen...';
+$lang['dl_fail'] = 'Herunterladen fehlgeschlagen.';
+$lang['dl_done'] = 'Herunterladen abgeschlossen (%s).';
+$lang['pk_extract'] = 'Archiv wird entpackt...';
+$lang['pk_fail']    = 'Entpacken fehlgeschlagen.';
+$lang['pk_done']    = 'Entpacken abgeschlossen.';
+$lang['pk_version'] = 'DokuWiki <b>%s</b> ist zur Installation bereit (Sie betreiben momentan <b>%s</b>).';
+$lang['ck_start']   = 'Dateirechte werden &uuml;berpr&uuml;ft...';
+$lang['ck_done']    = 'Alle Dateien sind beschreibbar. Zur Aktualisierung bereit.';
+$lang['ck_fail']    = 'Einige Dateien sind nicht beschreibbar. Die automatische Aktualisierung ist nicht m&ouml;glich.';
+$lang['cp_start']   = 'Dateien werden aktualisiert...';
+$lang['cp_done']    = 'Dateien wurden aktualisiert.';
+$lang['cp_fail']    = 'Irgendetwas funktioniert nicht. &Uuml;berpr&uuml;fen Sie von Hand.';
+$lang['tv_noperm']  = '<code>%s</code> ist nicht beschreibbar!';
+$lang['tv_upd']     = '<code>%s</code> wird aktualisiert.';
+$lang['tv_nocopy']  = 'Konnte Datei <code>%s</code> nicht kopieren!';
+$lang['tv_nodir']   = 'Konnte Verzeichnis <code>%s</code> nicht erstellen!';
+$lang['tv_done']    = '<code>%s</code> wurde aktualisiert.';
+$lang['rm_done']    = 'Veraltete <code>%s</code> wurde gel&ouml;scht.';
+$lang['rm_fail']    = 'Konnte veraltete Datei <code>%s</code> nicht l&ouml;schen. <b>Bitte l&ouml;schen Sie von Hand!</b>';
+$lang['finish']     = 'Aktualisierung abgeschlossen. Genie&szlig;en Sie Ihr neues DokuWiki!';
