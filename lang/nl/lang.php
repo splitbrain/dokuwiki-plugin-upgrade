6fe94487cbeba079a32f5a050ec31cc091fa319d
Gerrit Uitslag <klapinklapin@gmail.com>
2013-08-09 00:45:45 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+$lang['menu']                  = 'Wiki Upgrade';
+$lang['dl_from']               = 'Archief wordt van %s gedownload...';
+$lang['dl_fail']               = 'Download is mislukt.';
+$lang['dl_done']               = 'Download is compleet (%s)';
+$lang['pk_extract']            = 'Archief uitpakken...';
+$lang['pk_fail']               = 'Uitpakken mislukt.';
+$lang['pk_done']               = 'Uitpakken voltooit.';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> is klaar om geïnstalleerd te worden (Op dit moment heb je <b>%s</b> draaien).';
+$lang['ck_start']              = 'Bestandspermissies controleren...';
+$lang['ck_done']               = 'Alle bestanden zijn beschrijfbaar. Klaar om te upgraden.';
+$lang['ck_fail']               = 'Sommige bestanden zijn niet beschrijfbaar. Automatische upgrade is niet mogelijk.';
+$lang['cp_start']              = 'Bestanden updaten...';
+$lang['cp_done']               = 'Alle bestanden zijn geüpdatet.';
+$lang['cp_fail']               = 'Uh-Oh. Iets ging verkeerd. Het is beter om het handmatig te controleren.';
+$lang['tv_noperm']             = '<code>%s</code> is niet beschrijfbaar!';
+$lang['tv_upd']                = '<code>%s</code> zal worden geüpdatet.';
+$lang['tv_nocopy']             = 'Kon het bestand <code>%s</code> niet kopiëren!';
+$lang['tv_nodir']              = 'De map <code>%s</code> kan niet aangemaakt worden!';
+$lang['tv_done']               = '<code>%s</code> is geüpdatet.';
+$lang['rm_done']               = 'Verouderde <code>%s</code> verwijderd.';
+$lang['rm_fail']               = 'Verouderde <code>%s</code> kan niet worden verwijderd. <b>Verwijder alsjeblieft handmatig!</b>';
+$lang['finish']                = 'Upgrade is compleet. Geniet van je nieuwe DokuWiki';
