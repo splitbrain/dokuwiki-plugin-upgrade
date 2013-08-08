37e4a3161e9a4a6397cc08a76a7070615d2b6f38
Ralph Lauren Outlet Online <jywajkbxyx@hotmail.com>
2013-08-08 14:22:26 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,34 +1,9 @@
 <?php
+
 /**
- * English language file for upgrade plugin
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Andreas Gohr <andi@splitbrain.org>
+ * @author Ralph Lauren Outlet Online <jywajkbxyx@hotmail.com>
  */
-
-// menu entry for admin plugins
-$lang['menu'] = 'Wiki Upgrade';
-
-// custom language strings for the plugin
-$lang['dl_from'] = 'Downloading archive from %s...';
-$lang['dl_fail'] = 'Download failed.';
-$lang['dl_done'] = 'Download completed (%s).';
-$lang['pk_extract'] = 'Unpacking archive...';
-$lang['pk_fail']    = 'Unpacking failed.';
-$lang['pk_done']    = 'Unpacking completed.';
-$lang['pk_version'] = 'DokuWiki <b>%s</b> is ready to install (You\'re currently running <b>%s</b>).';
-$lang['ck_start']   = 'Checking file permissions...';
-$lang['ck_done']    = 'All files are writable. Ready to upgrade.';
-$lang['ck_fail']    = 'Some files aren\'t writable. Automatic upgrade not possible.';
-$lang['cp_start']   = 'Updating files...';
-$lang['cp_done']    = 'All files updated.';
-$lang['cp_fail']    = 'Uh-Oh. Something went wrong. Better check manually.';
-$lang['tv_noperm']  = '<code>%s</code> is not writable!';
-$lang['tv_upd']     = '<code>%s</code> will be updated.';
-$lang['tv_nocopy']  = 'Could not copy file <code>%s</code>!';
-$lang['tv_nodir']   = 'Could not create directory <code>%s</code>!';
-$lang['tv_done']    = 'updated <code>%s</code>';
-$lang['rm_done']    = 'Deprecated <code>%s</code> deleted.';
-$lang['rm_fail']    = 'Could not delete deprecated <code>%s</code>. <b>Please delete manually!</b>';
-$lang['finish']     = 'Upgrade completed. Enjoy your new DokuWiki';
-
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+$lang['menu']                  = 'Barclays and Deutsche Bank also had obtain ratings around the stock ahead of its filing. Ralph Lauren Outlet Online http://jonanddale.com/about.asp';
