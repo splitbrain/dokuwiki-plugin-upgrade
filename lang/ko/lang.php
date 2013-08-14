646dbba36f4802cb267cf027cce0d580a995f32e
Myeongjin <aranet100@gmail.com>
2013-08-14 08:40:42 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['menu']                  = '위키 업그레이드';
+$lang['dl_from']               = '%s에서 아카이브 다운로드 중...';
+$lang['dl_fail']               = '다운로드가 실패했습니다.';
+$lang['dl_done']               = '다운로드가 완료되었습니다. (%s)';
+$lang['pk_extract']            = '아카이브를 압축 푸는 중...';
+$lang['pk_fail']               = '압축 풀기가 실패했습니다.';
+$lang['pk_done']               = '압축 풀기가 완료되었습니다.';
+$lang['pk_version']            = '도쿠위키 <b>%s</b>(은)는 설치할 준비가 되어 있습니다. (현재 <b>%s</b>(을)를 실행하고 있습니다)';
+$lang['ck_start']              = '파일 권한 확인 중...';
+$lang['ck_done']               = '모든 파일을 쓸 수 있습니다. 업그레이드를 준비합니다.';
+$lang['ck_fail']               = '일부 파일을 쓸 수 없습니다. 자동으로 업그레이트를 할 수 없습니다.';
+$lang['cp_start']              = '파일을 업데이트 중...';
+$lang['cp_done']               = '모든 파일을 업데이트했습니다.';
+$lang['cp_fail']               = '어머나. 무언가가 잘못되었습니다. 수동으로 잘 확인하세요.';
+$lang['tv_noperm']             = '<code>%s</code>(은)는 쓸 수 없습니다!';
+$lang['tv_upd']                = '<code>%s</code>(은)는 업데이트됩니다.';
+$lang['tv_nocopy']             = '<code>%s</code> 파일을 복사할 수 없습니다!';
+$lang['tv_nodir']              = '<code>%s</code> 디렉터리를 만들 수 없습니다!';
+$lang['tv_done']               = '<code>%s</code>(을)를 업데이트했습니다';
+$lang['rm_done']               = '사용되지 않는 <code>%s</code>(은)는 삭제되었습니다.';
+$lang['rm_fail']               = '사용되지 않는 <code>%s</code>(을)를 삭제할 수 없습니다. <b>수동으로 삭제하세요!</b>';
+$lang['finish']                = '업그레이드가 완료되었습니다. 새 도쿠위키를 즐기세요';
