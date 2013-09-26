bf6f685cb9579b0160c67a93c3847c97e3ec2f62
Hideaki SAWADA <chuno@live.jp>
2013-09-26 19:25:06 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['menu']                  = 'Wiki のアップグレード';
+$lang['dl_from']               = '%s からアーカイブをダウンロード中...';
+$lang['dl_fail']               = 'ダウンロードが失敗しました。';
+$lang['dl_done']               = 'ダウンロードが完了しました（%s）。';
+$lang['pk_extract']            = 'アーカイブを解凍中...';
+$lang['pk_fail']               = '解凍が失敗しました。';
+$lang['pk_done']               = '解凍が完了しました。';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> をインストールする準備ができました（現在 <b>%s</b> を実行中です）。';
+$lang['ck_start']              = 'ファイル権限を確認中...';
+$lang['ck_done']               = '全ファイルが書込み可能です。アップグレードする準備ができました。';
+$lang['ck_fail']               = 'いくつかのファイルが書込不可です。自動アップグレードは不可能です。';
+$lang['cp_start']              = 'ファイルの更新中...';
+$lang['cp_done']               = '全ファイルの更新完了。';
+$lang['cp_fail']               = '何かが間違っていました。手動で確認して下さい。';
+$lang['tv_noperm']             = '<code>%s</code> は書込み不可です！';
+$lang['tv_upd']                = '<code>%s</code> は更新可能です。';
+$lang['tv_nocopy']             = '<code>%s</code> ファイルがコピーできませんでした！';
+$lang['tv_nodir']              = '<code>%s</code> ディレクトリが作成できませんでした！';
+$lang['tv_done']               = '<code>%s</code> の更新完了。';
+$lang['rm_done']               = '廃止予定の <code>%s</code> の削除完了。';
+$lang['rm_fail']               = '廃止予定の <code>%s</code> が削除できませんでした。<b>手動で削除して下さい！</b>';
+$lang['finish']                = 'アップグレードが完了しました。新しい DokuWiki をお楽しみ下さい。';
