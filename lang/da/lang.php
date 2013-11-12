cd0f635276efb24ae067ffdbf76a644c662bb035
soer9648 <soer9648@eucl.dk>
2013-11-12 20:54:23 +0100

    translation update

---
+++ lang/da/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author soer9648 <soer9648@eucl.dk>
+ */
+$lang['menu']                  = 'Wiki Opgradering';
+$lang['dl_from']               = 'Downloader arkiv fra %s...';
+$lang['dl_fail']               = 'Download fejlet';
+$lang['dl_done']               = 'Download færdig (%s).';
+$lang['pk_extract']            = 'Pakker arkiv ud...';
+$lang['pk_fail']               = 'Udpakning fejlet.';
+$lang['pk_done']               = 'Udpakning færdig.';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> er klar til installation (Du benytter i øjeblikket <b>%s</b>).';
+$lang['ck_start']              = 'Kontrollerer filtilladelser';
+$lang['ck_done']               = 'Alle filer er skrivbare. Klar til at opgradere.';
+$lang['ck_fail']               = 'Nogle filer er ikke skrivbare. Automatisk opgradering er ikke muligt.';
+$lang['cp_start']              = 'Opdaterer filer...';
+$lang['cp_done']               = 'Alle filer opdateret.';
+$lang['cp_fail']               = 'Å-Åh. Noget gik galt. Du må hellere tjekke manuelt.';
+$lang['tv_noperm']             = '<code>%s</code> er ikke skrivbar!';
+$lang['tv_upd']                = '<code>%s</code> vil blive opdateret.';
+$lang['tv_nocopy']             = 'Kunne ikke kopiere filen <code>%s</code>!';
+$lang['tv_nodir']              = 'Kunne ikke oprette mappen <code>%s</code>!';
+$lang['tv_done']               = 'Opdaterede <code>%s</code>';
+$lang['rm_done']               = 'Forældet <code>%s</code> slettet.';
+$lang['rm_fail']               = 'Kunne ikke slette forældet <code>%s</code>. <b>Slet venligst manuelt!</b>';
+$lang['finish']                = 'Opgradering færdig. Nyd din nye DokuWiki';
