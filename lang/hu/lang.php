d148bc9933fa8c3e410c9a3ffe0d969129e89187
Marina Vladi <deldadam@gmail.com>
2014-01-22 23:00:21 +0100

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['menu']                  = 'Wiki-frissítő';
+$lang['dl_from']               = 'Archívum letöltése innen: %s...';
+$lang['dl_fail']               = 'A letöltés sikertelen.';
+$lang['dl_done']               = 'A letöltés befejeződött (%s).';
+$lang['pk_extract']            = 'Archívum kicsomagolása...';
+$lang['pk_fail']               = 'A kicsomagolás sikertelen.';
+$lang['pk_done']               = 'A kicsomagolás befejeződött.';
+$lang['pk_version']            = 'A DokuWiki <b>%s</b> készen áll a telepítésre. (Jelenleg telepítve: b>%s</b>)';
+$lang['ck_start']              = 'Fájlok hozzáférési jogosultságainak ellenőrzése...';
+$lang['ck_done']               = 'Minden fájl írható. A frissítés készen áll a telepítésre.';
+$lang['ck_fail']               = 'Néhány fájl nem írható. Az automatikus frissítés nem lehetséges.';
+$lang['cp_start']              = 'Fájlok frissítése...';
+$lang['cp_done']               = 'Minden fájl frissítve.';
+$lang['cp_fail']               = 'Ejha. Valami nem sikerült. Jobb, ha manuálisan ellenőrizzük.';
+$lang['tv_noperm']             = 'A(z) <code>%s</code> nem írható!';
+$lang['tv_upd']                = 'A(z) <code>%s</code> frissítésre kerül!';
+$lang['tv_nocopy']             = 'Nem tudtam lemásolni a(z) <code>%s</code> nevű fájlt!';
+$lang['tv_nodir']              = 'Nem tudtam létrehozni a(z) <code>%s</code> nevű könyvtárat!';
+$lang['tv_done']               = '<code>%s</code> frissítve';
+$lang['rm_done']               = 'Elavult fájl törölve: <code>%s</code>.';
+$lang['rm_fail']               = 'Nem tudtam törölni az elavult fájlt: <code>%s</code>. <b>Töröljük manuálisan!</b>';
+$lang['finish']                = 'Frissítés kész. Élvezzük az új DokuWiki-t!';
