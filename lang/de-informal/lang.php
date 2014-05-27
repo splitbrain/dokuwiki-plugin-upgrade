bbebf082730511fb3394b5d3ba9b8eb73f8a24b4
Thomas Templin <templin@gnuwhv.de>
2014-05-27 14:00:06 +0200

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thomas Templin <templin@gnuwhv.de>
+ */
+$lang['menu']                  = 'Wiki aktualisieren';
+$lang['dl_from']               = 'Archiv wird von %s heruntergeladen...';
+$lang['dl_fail']               = 'Herunterladen fehlgeschlagen.';
+$lang['dl_done']               = 'Herunterladen abgeschlossen (%s).';
+$lang['pk_extract']            = 'Archiv wird entpackt...';
+$lang['pk_fail']               = 'Entpacken fehlgeschlagen.';
+$lang['pk_done']               = 'Entpacken abgeschlossen.';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> ist zur Installation bereit (Du betreibst momentan <b>%s</b>).';
+$lang['ck_start']              = 'Dateirechte werden &uuml;berpr&uuml;ft...';
+$lang['ck_done']               = 'Alle Dateien sind beschreibbar. Zur Aktualisierung bereit.';
+$lang['ck_fail']               = 'Einige Dateien sind nicht beschreibbar. Die automatische Aktualisierung ist nicht m&ouml;glich.';
+$lang['cp_start']              = 'Dateien werden aktualisiert...';
+$lang['cp_done']               = 'Dateien wurden aktualisiert.';
+$lang['cp_fail']               = 'Autsch. Irgendetwas funktioniert nicht. &Uuml;berpr&uuml;fe es besser von Hand.';
+$lang['tv_noperm']             = '<code>%s</code> ist nicht beschreibbar!';
+$lang['tv_upd']                = '<code>%s</code> wird aktualisiert.';
+$lang['tv_nocopy']             = 'Konnte Datei <code>%s</code> nicht kopieren!';
+$lang['tv_nodir']              = 'Konnte Verzeichnis <code>%s</code> nicht erstellen!';
+$lang['tv_done']               = '<code>%s</code> wurde aktualisiert.';
+$lang['rm_done']               = 'Veraltete <code>%s</code> wurde gel&ouml;scht.';
+$lang['rm_fail']               = 'Konnte veraltete Datei <code>%s</code> nicht l&ouml;schen. <b>Bitte l&ouml;schen Sie von Hand!</b>';
+$lang['finish']                = 'Aktualisierung abgeschlossen. Genie&szlig;en Sie Ihr neues DokuWiki!';
