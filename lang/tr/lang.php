feb94379fc2c054a71d28dde5196e55bf84ce404
İlker R. Kapaç <irifat@gmail.com>
2014-06-10 23:35:07 +0200

    translation update

---
+++ lang/tr/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author İlker R. Kapaç <irifat@gmail.com>
+ */
+$lang['menu']                  = 'Wiki Yükseltme';
+$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
+$lang['dl_fail']               = 'İndirme başarısız oldu.';
+$lang['dl_done']               = 'İndirme tamamlandı (%s).';
+$lang['pk_extract']            = 'Arşiv açılıyor...';
+$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
+$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
+$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
+$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
+$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
+$lang['cp_start']              = 'Dosyalar güncelleniyor...';
+$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
+$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
+$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
+$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
+$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
+$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
+$lang['tv_done']               = '<code>%s</code> güncellendi';
+$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
+$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
+$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
