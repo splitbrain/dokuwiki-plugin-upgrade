86d8cf518eda003b48358fe25c10bbbaa93f0fb2
Peter van Diest <peter.van.diest@xs4all.nl>
2014-07-21 14:20:34 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,21 +4,31 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author Peter van Diest <peter.van.diest@xs4all.nl>
  */
 $lang['menu']                  = 'Wiki Upgrade';
+$lang['vs_php']                = 'New Dokuwiki releases hebben minstens PHP %s nodig, maar je gebruikt  %s. Je moet eerst je PHP-versie vernieuwen voor je Dokuwiki vernieuwt.';
+$lang['vs_tgzno']              = 'Kon niet de laatste versie van Dokuwiki bepalen.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> is beschikbaar voor download.';
+$lang['vs_local']              = 'Je gebruikt nu DokuWiki <b>%s</b>.';
+$lang['vs_localno']            = 'Het is niet duidelijk hoe oud de versie is die je nu gebruikt, handmatige upgrade is aan te raden.';
+$lang['vs_newer']              = 'Het lijkt erop dat je huidige Dokuwiki nog nieuwer is dan de laatste stabiele release. Een upgrade is niet aan te raden.';
+$lang['vs_same']               = 'Je huidige Dokuwiki is al up-to-date. Een upgrade is niet nodig.';
+$lang['vs_plugin']             = 'Er is een nieuwere upgrade plugin beschikbaar (%s), je kunt de plugin beter vernieuwen voor je verder gaat.';
+$lang['vs_ssl']                = 'Je PHP lijkt SSL-streams niet te ondersteunen, het downloaden van de benodigde data zal waarschijnlijk misgaan. Voer  een handmatige upgrade uit.';
 $lang['dl_from']               = 'Archief wordt van %s gedownload...';
 $lang['dl_fail']               = 'Download is mislukt.';
 $lang['dl_done']               = 'Download is compleet (%s)';
 $lang['pk_extract']            = 'Archief uitpakken...';
 $lang['pk_fail']               = 'Uitpakken mislukt.';
-$lang['pk_done']               = 'Uitpakken voltooit.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> is klaar om geïnstalleerd te worden (Op dit moment heb je <b>%s</b> draaien).';
+$lang['pk_done']               = 'Uitpakken voltooid.';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> is klaar om geïnstalleerd te worden. (Op dit moment gebnruik je <b>%s</b>.)';
 $lang['ck_start']              = 'Bestandspermissies controleren...';
 $lang['ck_done']               = 'Alle bestanden zijn beschrijfbaar. Klaar om te upgraden.';
 $lang['ck_fail']               = 'Sommige bestanden zijn niet beschrijfbaar. Automatische upgrade is niet mogelijk.';
 $lang['cp_start']              = 'Bestanden updaten...';
 $lang['cp_done']               = 'Alle bestanden zijn geüpdatet.';
-$lang['cp_fail']               = 'Uh-Oh. Iets ging verkeerd. Het is beter om het handmatig te controleren.';
+$lang['cp_fail']               = 'Uh-Oh. Er ging iets fout. Het is beter om het handmatig te controleren.';
 $lang['tv_noperm']             = '<code>%s</code> is niet beschrijfbaar!';
 $lang['tv_upd']                = '<code>%s</code> zal worden geüpdatet.';
 $lang['tv_nocopy']             = 'Kon het bestand <code>%s</code> niet kopiëren!';
@@ -27,3 +37,10 @@ $lang['tv_done']               = '<code>%s</code> is geüpdatet.';
 $lang['rm_done']               = 'Verouderde <code>%s</code> verwijderd.';
 $lang['rm_fail']               = 'Verouderde <code>%s</code> kan niet worden verwijderd. <b>Verwijder alsjeblieft handmatig!</b>';
 $lang['finish']                = 'Upgrade is compleet. Geniet van je nieuwe DokuWiki';
+$lang['btn_continue']          = 'Doorgaan';
+$lang['btn_abort']             = 'Afbreken';
+$lang['step_version']          = 'Controleer';
+$lang['step_download']         = 'Download';
+$lang['step_unpack']           = 'Pak uit';
+$lang['step_check']            = 'Verifiëer';
+$lang['step_upgrade']          = 'Installleer';
