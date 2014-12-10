001463ae1427361bc00de84a487516e0ca7d4564
Søren Birk <soer9648@eucl.dk>
2014-12-10 10:10:07 +0100

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -4,8 +4,18 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author soer9648 <soer9648@eucl.dk>
+ * @author Søren Birk <soer9648@eucl.dk>
  */
 $lang['menu']                  = 'Wiki Opgradering';
+$lang['vs_php']                = 'Nye DokuWiki udgivelser kræver som minimum PHP %s, men du benytter i øjeblikket %s. Du bør opdatere din PHP-version, før du opgraderer!';
+$lang['vs_tgzno']              = 'Kunne ikke fastlægge den nyeste version af DokuWiki.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> er klar til download.';
+$lang['vs_local']              = 'Du benytter i øjeblikket DokuWiki <b>%s</b>.';
+$lang['vs_localno']            = 'Det er ikke klart, hvor gammel din nuværende version er. Manuel opgradering anbefales.';
+$lang['vs_newer']              = 'Det ser ud til at din nuværende DokuWiki er nyere end den nyeste stabile version. Opgradering anbefales ikke.';
+$lang['vs_same']               = 'Din nuværende DokuWiki er allerede ajourført. Det er ikke nødvendigt at opgradere.';
+$lang['vs_plugin']             = 'Der er et nyere opgraderingsplugin tilgængeligt (%s). Du bør opdatere dit plugin, før du fortsætter.';
+$lang['vs_ssl']                = 'Det ser ud til at dit PHP ikke supportere SSL streams - download af nødvendigt data vil højst sandsynligt fejle. Opgradér manuelt i stedet.';
 $lang['dl_from']               = 'Downloader arkiv fra %s...';
 $lang['dl_fail']               = 'Download fejlet';
 $lang['dl_done']               = 'Download færdig (%s).';
@@ -27,3 +37,10 @@ $lang['tv_done']               = 'Opdaterede <code>%s</code>';
 $lang['rm_done']               = 'Forældet <code>%s</code> slettet.';
 $lang['rm_fail']               = 'Kunne ikke slette forældet <code>%s</code>. <b>Slet venligst manuelt!</b>';
 $lang['finish']                = 'Opgradering færdig. Nyd din nye DokuWiki';
+$lang['btn_continue']          = 'Fortsæt';
+$lang['btn_abort']             = 'Afbryd';
+$lang['step_version']          = 'Tjek';
+$lang['step_download']         = 'Download';
+$lang['step_unpack']           = 'Pak Ud';
+$lang['step_check']            = 'Verificér';
+$lang['step_upgrade']          = 'Installér';
