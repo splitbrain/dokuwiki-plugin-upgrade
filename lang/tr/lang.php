67af105516a71df067763ba00bade34bd738ebf4
İlker R. Kapaç <irifat@gmail.com>
2015-05-26 23:25:38 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -6,6 +6,15 @@
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
 $lang['menu']                  = 'Wiki Yükseltme';
+$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
+$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
+$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
+$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
+$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
+$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
+$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
+$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
 $lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
 $lang['dl_fail']               = 'İndirme başarısız oldu.';
 $lang['dl_done']               = 'İndirme tamamlandı (%s).';
@@ -27,3 +36,10 @@ $lang['tv_done']               = '<code>%s</code> güncellendi';
 $lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
 $lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
 $lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
+$lang['btn_continue']          = 'Devam et';
+$lang['btn_abort']             = 'İptal';
+$lang['step_version']          = 'Kontrol et';
+$lang['step_download']         = 'İndir';
+$lang['step_unpack']           = 'Doayaları çıkar';
+$lang['step_check']            = 'Doğrula';
+$lang['step_upgrade']          = 'Kur';
