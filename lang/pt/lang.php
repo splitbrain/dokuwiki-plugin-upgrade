8a7998bfeac86d6d11c452b515447c9fb32f526a
José Vieira <jmsv63@gmail.com>
2018-09-27 19:10:05 +0200

    translation update

--- lang/pt/lang.php
+++ lang/pt/lang.php
@@ -2,27 +2,28 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author José Vieira <jmsv63@gmail.com>
  * @author Oze Projetos <oze@oze.net.br>
  */
 $lang['menu']                  = 'Atualização Wiki';
-$lang['vs_php']                = 'A nova versão do DokuWiki necessita ao menos PHP %s, mas você está utilizando %s.
-Você deve fazer atualizar sua versão do PHP antes de continuar a atualização!';
-$lang['vs_tgzno']              = 'Não foi possível determinar a versão mais nova do DokuWiki.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> está disponível para download.';
-$lang['vs_local']              = 'Você está atualmente rodando DokuWiki  <b>%s</b>.';
-$lang['vs_localno']            = 'Não está claro qual a sua versão, é recomendado a atualização manual.';
+$lang['vs_php']                = 'A nova versão de DokuWiki necessita pelo menos de PHP %s, mas está utilizando %s.
+Terá de atualizar a sua versão do PHP antes de continuar a atualização!';
+$lang['vs_tgzno']              = 'Não foi possível determinar a versão mais recente de DokuWiki.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> está disponível para descarregar.';
+$lang['vs_local']              = 'Atualmente está a executar DokuWiki  <b>%s</b>.';
+$lang['vs_localno']            = 'Não é claro qual a sua versão, sendo recomendada uma atualização manual.';
 $lang['vs_newer']              = 'Parece que seu DokuWiki  é mais recente que a última versão estável. Atualização não recomendável.';
 $lang['vs_same']               = 'Seu DokuWiki já é a versão mais recente. Não necessita de atualização.';
 $lang['vs_plugin']             = 'Há uma nova atualização de plugin disponível (%s) você deve atualizar o plugin antes de continuar.';
 $lang['vs_ssl']                = 'Seu PHP parece não suportar SSL, download da informação necessária irá falhar. Atualize manualmente.';
-$lang['dl_from']               = 'Fazendo download de %s...';
-$lang['dl_fail']               = 'Falha de download.';
-$lang['dl_done']               = 'Download completo (%s).';
+$lang['dl_from']               = 'Descarregando arquivo de %s...';
+$lang['dl_fail']               = 'Descarga falhou.';
+$lang['dl_done']               = 'Descarga completa (%s).';
 $lang['pk_extract']            = 'Descompactando...';
 $lang['pk_fail']               = 'Falha de descompactação.';
 $lang['pk_done']               = 'Descompactação completada.';
-$lang['pk_version']            = 'DokuWiki  <b>%s</b> já está instalado (Você está rodando a versão <b>%s</b>).';
+$lang['pk_version']            = 'DokuWiki  <b>%s</b> já está instalado (Você está executando a versão <b>%s</b>).';
 $lang['ck_start']              = 'Verificando permissões de arquivo ...';
 $lang['ck_done']               = 'Todos os arquivos são graváveis. Pronto para atualizar.';
 $lang['ck_fail']               = 'Alguns arquivos não são graváveis. Não é possível a atualização automática.';
@@ -34,12 +35,13 @@ $lang['tv_upd']                = '<code>%s</code> será atualizado.';
 $lang['tv_nocopy']             = 'Não foi possível copiar o arquivo <code>%s</code>!';
 $lang['tv_nodir']              = 'Não foi possível criar o diretório <code>%s</code>!';
 $lang['tv_done']               = '<code>%s</code> atualizado!';
-$lang['rm_done']               = '<code>%s</code> obsoleto excluído.';
-$lang['rm_fail']               = 'Não foi possível excluir <code>%s</code> obsoleto. <B> Por favor, apague manualmente! </ B>';
+$lang['rm_done']               = '<code>%s</code> obsoleto foi eliminado.';
+$lang['rm_fail']               = 'Não foi possível excluir <code>%s</code> obsoleto. <B> Por favor, eliminar manualmente! </ B>';
+$lang['rm_mismatch']           = 'Incompatibilidade com maiúsculas ou minúsculas no ficheiro obsoleto <code>%s</code>. Por favor verificar manualmente se este ficheiro deverá ser eliminado.';
 $lang['finish']                = 'Atualizar concluída. Aproveite o seu novo DokuWiki';
 $lang['btn_continue']          = 'Continue';
 $lang['btn_abort']             = 'Abortar';
-$lang['step_version']          = 'Checar';
+$lang['step_version']          = 'Conferir';
 $lang['step_download']         = 'Download';
 $lang['step_unpack']           = 'Descompactar';
 $lang['step_check']            = 'Verificar';
