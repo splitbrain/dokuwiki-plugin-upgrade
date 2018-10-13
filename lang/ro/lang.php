73d7a4b0559f1bbefbcc63ea4b5bb12e78989ba1
Crăciun Petrișor <petrisor.craciun34@gmail.com>
2018-10-13 15:30:38 +0200

    translation update

---
+++ lang/ro/lang.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Crăciun Petrișor <petrisor.craciun34@gmail.com>
+ */
+$lang['menu']                  = 'Actualizare Wiki ';
+$lang['vs_php']                = 'Noua versiune DokuWiki necesită cel puțin versiunea de PHP %s.  Dumneavoastră rulați PHP %s. Ar trebui să actualizați versiunea dvs. de PHP înainte de a face actualizarea!';
+$lang['vs_tgzno']              = 'Nu s-a putut instala cea mai nouă versiune de DokuWiki.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> este disponibil pentru descăcare.';
+$lang['vs_local']              = 'În prezent, folosiți DokuWiki <b>%s</b>.';
+$lang['vs_localno']            = 'Nu este clar cât de veche este versiunea curentă, este recomandată actualizarea manuală.';
+$lang['vs_newer']              = 'Se pare că actuala versiune de DokuWiki corespunde cu ultima versiune stabilă. Actualizarea nu este recomandată. ';
+$lang['vs_same']               = 'Versiunea dumneavoastră de DokuWiki este deja actualizată.Ne este nevoie de actualizare.';
+$lang['vs_plugin']             = 'Există o actualizare disponibilă pentru un plugin (% s), trebuie să actualizați plugin-ul înainte de a continua.';
+$lang['vs_ssl']                = 'Versiunea dv. de PHP-ul  nu suportă fluxurile SSL, descărcarea datelor necesare va eșua cel mai probabil. Încercați sa faceți upgrade manual.';
+$lang['dl_from']               = 'Descărcarea arhivei %s...';
+$lang['dl_fail']               = 'Descărcare eșuată.
+';
+$lang['dl_done']               = 'Descărcare completă (%s).';
+$lang['pk_extract']            = 'Dezarhivarea arhivei ...';
+$lang['pk_fail']               = 'Dezarhivarea arhivei nu a reușit.';
+$lang['pk_done']               = 'Dezarhivare compeltă.';
+$lang['pk_version']            = 'DokuWiki <b>%s</b> este gata de instalare (În prezent, folosiți <b>%s</b>).';
+$lang['ck_start']              = 'Se verifică permisiunile fișierului ...';
+$lang['ck_done']               = 'Toate fișierele sunt scrise. Gata de actualizare.';
+$lang['ck_fail']               = 'Unele fișiere nu pot fi inscripționate. Actualizarea automată nu este posibilă.';
+$lang['cp_start']              = 'Actualizarea fișierelor ...';
+$lang['cp_done']               = 'Toate fișierele au fost actualizate.';
+$lang['cp_fail']               = 'Uh-Oh. Ceva n-a mers bine.Încercați manual.';
+$lang['tv_noperm']             = '<code>%s</code> nu poate fi scris!';
+$lang['tv_upd']                = '<code>%s</code> va fi actualizat.';
+$lang['tv_nocopy']             = 'Nu s-a putut elimina fișierul existent <code>%s</ code> înainte de suprascriere!';
+$lang['tv_nodir']              = 'Nu s-a putut crea directorul <code>%s</code>!';
+$lang['tv_done']               = 'actualizare <code>% s </ code>';
+$lang['rm_done']               = 'Problema <code>%s</code> a fost ștersă.';
+$lang['rm_fail']               = 'Nu s-a putut șterge codul <code>% s</ code>. <b> Vă rugăm să ștergeți manual! </ b>';
+$lang['rm_mismatch']           = ' Verificați manual dacă fișierul  <code>%s</code> ar trebui să fie într-adevăr șters.';
+$lang['finish']                = 'Actualizarea a fost finalizată. Bucurați-vă de noul DokuWiki !';
+$lang['btn_continue']          = 'Continuă';
+$lang['btn_abort']             = 'Renunță';
+$lang['step_version']          = 'Verifică';
+$lang['step_download']         = 'Descarcă';
+$lang['step_unpack']           = 'Dezarhivează';
+$lang['step_check']            = 'Verifică';
+$lang['step_upgrade']          = 'Instalare';
+$lang['careful']               = 'Au aparut erori !! Ar trebuie să <b>nu</b> continuați !';
