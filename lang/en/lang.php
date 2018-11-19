50eee846143e8d37e977e0bae8d80a289a060b44
Katerina Katapodi <extragold1234@hotmail.com>
2018-11-19 14:25:28 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,57 +1,50 @@
 <?php
+
 /**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
  * English language file for upgrade plugin
  *
+ * @author Katerina Katapodi <extragold1234@hotmail.com>
  * @author Andreas Gohr <andi@splitbrain.org>
  */
-
-// menu entry for admin plugins
-$lang['menu'] = 'Wiki Upgrade';
-
-// custom language strings for the plugin
-$lang['vs_php']     = 'New DokuWiki releases need at least PHP %s, but you\'re running %s. You should upgrade your PHP version before upgrading!';
-$lang['vs_tgzno']   = 'Could not determine the newest version of DokuWiki.';
-$lang['vs_tgz']     = 'DokuWiki <b>%s</b> is available for download.';
-$lang['vs_local']   = 'You\'re currently running DokuWiki <b>%s</b>.';
-$lang['vs_localno'] = 'It\'s not clear how old your currently running version is, manual upgrade is recommended.';
-$lang['vs_newer']   = 'It seems your current DokuWiki is even newer than the latest stable release. Upgrade not recommended.';
-$lang['vs_same']    = 'Your current DokuWiki is already up to date. No need for upgrading.';
-$lang['vs_plugin']  = 'There is a newer upgrade plugin available (%s) you should update the plugin before continuing.';
-$lang['vs_ssl']     = 'Your PHP seems not to support SSL streams, downloading the needed data will most likely fail. Upgrade manually instead.';
-
-$lang['dl_from']    = 'Downloading archive from %s...';
-$lang['dl_fail']    = 'Download failed.';
-$lang['dl_done']    = 'Download completed (%s).';
-$lang['pk_extract'] = 'Unpacking archive...';
-$lang['pk_fail']    = 'Unpacking failed.';
-$lang['pk_done']    = 'Unpacking completed.';
-$lang['pk_version'] = 'DokuWiki <b>%s</b> is ready to install (You\'re currently running <b>%s</b>).';
-$lang['ck_start']   = 'Checking file permissions...';
-$lang['ck_done']    = 'All files are writable. Ready to upgrade.';
-$lang['ck_fail']    = 'Some files aren\'t writable. Automatic upgrade not possible.';
-$lang['cp_start']   = 'Updating files...';
-$lang['cp_done']    = 'All files updated.';
-$lang['cp_fail']    = 'Uh-Oh. Something went wrong. Better check manually.';
-$lang['tv_noperm']  = '<code>%s</code> is not writable!';
-$lang['tv_upd']     = '<code>%s</code> will be updated.';
-$lang['tv_nocopy']  = 'Could not copy file <code>%s</code>!';
-$lang['tv_nocopy']  = 'Could not remove existing file <code>%s</code> before overwriting!';
-$lang['tv_nodir']   = 'Could not create directory <code>%s</code>!';
-$lang['tv_done']    = 'updated <code>%s</code>';
-$lang['rm_done']    = 'Deprecated <code>%s</code> deleted.';
-$lang['rm_fail']    = 'Could not delete deprecated <code>%s</code>. <b>Please delete manually!</b>';
-$lang['rm_mismatch']= 'Case mismatch for deprecated file <code>%s</code>. Please check manually if file should really be deleted.';
-$lang['finish']     = 'Upgrade completed. Enjoy your new DokuWiki';
-
-$lang['btn_continue'] = 'Continue';
-$lang['btn_abort']    = 'Abort';
-
-$lang['step_version']  = 'Check';
-$lang['step_download'] = 'Download';
-$lang['step_unpack']   = 'Unpack';
-$lang['step_check']    = 'Verify';
-$lang['step_upgrade']  = 'Install';
-
-$lang['careful'] = 'Errors above! You should <b>not</b> continue!';
-
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+$lang['menu']                  = 'Αναβάθμιση του Wiki';
+$lang['vs_php']                = 'Οι συνεισφορές του DokuWiki χρειάζονται PHP %s,τουλάχιστον αλλά λειτουργεί με  %s. Πρέπει να αναβαθμίσετε το PHP όπως είναι πριν την αναβάθμιση !';
+$lang['vs_tgzno']              = 'Δεν μπόρεσε να προσδιορίσει την πιο πρόσφατη εκδοχή του  DokuWiki.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s</b> είναι διαθέσιμο να κατεβάσετε από τον υπολογιστή.';
+$lang['vs_local']              = 'Αυτή την στιγμή χειρίζεστε την λειτουργία του DokuWiki <b>%s</b>.';
+$lang['vs_localno']            = 'Δεν είναι σαφές πόσο παλιά είναι η παρούσα μορφή/εκδοχή , συστήνεται χειροκίνητη αναβάθμιση';
+$lang['vs_newer']              = 'Φαίνεται πως το παρόν DokuWiki είναι πολύ νεότερο από την τελευταία του μορφή. Δεν χρειάζεται αναβάθμιση. ';
+$lang['vs_same']               = 'Το παρόν σας  DokuWiki είναι ήδη νέο. Δεν χρειάζεται αναβάθμιση.';
+$lang['vs_plugin']             = 'Υπάρχει ένα νέο πρόσθετο διαθέσιμο  (%s) που πρέπει να ενημερώσετε πριν συνεχίσετε.';
+$lang['vs_ssl']                = 'Φαίνεται πως το PHP δεν υποστηρίζει λειτουργία του SSL , το κατέβασμα  δεδομένων υπολογιστή μάλλον θα  αποτύχει. Χρειάζεται αντιθέτως αναβάθμιση με το χέρι. ';
+$lang['dl_from']               = 'Κατέβασμα αρχείου από %s...';
+$lang['dl_fail']               = 'Το κατέβασμα από τον υπολογιστή απέτυχε.';
+$lang['dl_done']               = 'Το κατέβασμα από τον υπολογιστή ολοκληρώθηκε(%s).';
+$lang['pk_extract']            = 'Διαδικασία κατεβάσματος από τον υπολογιστή..';
+$lang['pk_fail']               = 'Το κατέβασμα απέτυχε.';
+$lang['pk_done']               = 'Η διαδικασία (ανοίγματος και κατεβάσματος από τον υπολογιστή)ολοκληρώθηκε.';
+$lang['pk_version']            = ' Το DokuWiki <b>%s</b> είναι έτοιμο προς εγκατάσταση. (Προς το παρόν γίνεται ολοκλήρωση του <b>%s</b>).';
+$lang['ck_start']              = 'Έλεγχος αδειών...';
+$lang['ck_done']               = 'Μπορείς να γράψεις σε όλα τα αρχεία.. Έτοιμο προς  αναβάθμιση ';
+$lang['ck_fail']               = 'Some files aren\'t writable. Automatic upgrade not possible.';
+$lang['cp_start']              = 'Updating files...';
+$lang['cp_done']               = 'All files updated.';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '<code>%s</code> is not writable!';
+$lang['tv_upd']                = '<code>%s</code> will be updated.';
+$lang['tv_nocopy']             = 'Could not remove existing file <code>%s</code> before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory <code>%s</code>!';
+$lang['tv_done']               = 'updated <code>%s</code>';
+$lang['rm_done']               = 'Deprecated <code>%s</code> deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated <code>%s</code>. <b>Please delete manually!</b>';
+$lang['rm_mismatch']           = 'Case mismatch for deprecated file <code>%s</code>. Please check manually if file should really be deleted.';
+$lang['finish']                = 'Upgrade completed. Enjoy your new DokuWiki';
+$lang['btn_continue']          = 'Continue';
+$lang['btn_abort']             = 'Abort';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'Download';
+$lang['step_unpack']           = 'Unpack';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'Install';
+$lang['careful']               = 'Errors above! You should <b>not</b> continue!';
