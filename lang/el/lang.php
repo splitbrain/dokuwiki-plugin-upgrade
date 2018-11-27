908f7d6e9a5ec8847d7803a627e0cca4a5d1f6d7
Katerina Katapodi <extragold1234@hotmail.com>
2018-11-27 12:15:26 +0100

    translation update

---
+++ lang/el/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <extragold1234@hotmail.com>
+ */
+$lang['btn_continue']          = 'Συνέχεια';
+$lang['btn_abort']             = 'Εξαγωγή';
+$lang['step_version']          = 'Έλεγχος ';
+$lang['step_download']         = 'Κατέβασμα';
+$lang['step_unpack']           = 'Λήψη';
+$lang['step_check']            = 'Επαλήθευση';
+$lang['step_upgrade']          = 'Εγκατάσταση';
+$lang['careful']               = 'Υπάρχουν λάθη πιο πάνω! Δεν πρέπει <b> </b> να συνεχίσετε!';
