d4ce89cbb0b5347b80c2a91c50110150aa78192a
Krzysiek <krzysztof.janiczek@gmail.com>
2019-05-26 14:55:04 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Krzysiek <krzysztof.janiczek@gmail.com>
+ */
+$lang['menu']                  = 'Aktualizacja Wiki';
+$lang['vs_php']                = 'Nowe wydanie DokuWiki wymaga co najmniej PHP %s, aposiadan wersja to %s. Przed uaktualnieniem powinieneś zaktualizować swoją wersję PHP!';
+$lang['vs_tgzno']              = 'Nie można określić najnowszej wersji DokuWiki.';
+$lang['vs_tgz']                = 'DokuWiki <b>%s </b> jest dostępna do pobrania.';
+$lang['vs_local']              = 'Aktualnie używasz DokuWiki <b>%s </b>.';
+$lang['vs_localno']            = 'Nie można określić, jak stara jest aktualnie uruchomiona wersja, zaleca się ręczną aktualizację.';
+$lang['vs_newer']              = 'Wygląda na to, że twoje obecne DokuWiki jest nowsze niż najnowsza stabilna wersja. Nie zaleca się uaktualnienia.';
+$lang['vs_same']               = 'Twoja wersja DokuWiki jest już aktualne. Nie ma potrzeby aktualizacji.';
