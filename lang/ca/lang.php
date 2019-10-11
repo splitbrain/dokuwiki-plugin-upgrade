8a4830f2110dc05a9b95d45e5f5b1bc4f7e79b9a
Adolfo Jayme Barrientos <fito@libreoffice.org>
2019-10-11 18:15:12 +0200

    translation update

---
+++ lang/ca/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Adolfo Jayme Barrientos <fito@libreoffice.org>
+ */
+$lang['vs_tgzno']              = 'No s’ha pogut determinar la darrera versió del DokuWiki.';
+$lang['dl_fail']               = 'Ha fallat la baixada.';
+$lang['cp_start']              = 'S’estan actualitzant els fitxers…';
+$lang['cp_done']               = 'S’han actualitzat tots els fitxers.';
+$lang['btn_continue']          = 'Continua';
+$lang['btn_abort']             = 'Interromp';
+$lang['step_version']          = 'Comprova';
+$lang['step_download']         = 'Baixa';
+$lang['step_unpack']           = 'Desempaqueta';
+$lang['step_check']            = 'Verifica';
+$lang['step_upgrade']          = 'Instal·la';
