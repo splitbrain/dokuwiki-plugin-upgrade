ea3958c9f362022868c06a908f7cce223d88e467
roro7amad <rrrrrr23r@gmail.com>
2020-03-26 12:15:34 +0100

    translation update

--- lang/ar/lang.php
+++ lang/ar/lang.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author roro7amad <rrrrrr23r@gmail.com>
  * @author Khalid <khalid.aljahil@gmail.com>
  */
 $lang['menu']                  = 'ترقية ويكي';
@@ -12,3 +13,6 @@ $lang['vs_tgzno']              = 'لم نستطع تحديد أحدث إصدار
 $lang['vs_tgz']                = 'دوكو ويكي <b>%s</b> متوفر للتحميل.';
 $lang['vs_local']              = 'الإصدار المستخدم حالياً هو <b>%s</b>.';
 $lang['vs_localno']            = 'لا يتضح لنا ماهو الإصدار المستخدم لديكم, من المستحسن عمل ترقية يدوية.';
+$lang['vs_newer']              = 'عربى
+يبدو أن DokuWiki الحالي الخاص بك هو أحدث حتى من أحدث إصدار مستقر. الترقية غير مستحسن.	';
+$lang['vs_same']               = '111';
