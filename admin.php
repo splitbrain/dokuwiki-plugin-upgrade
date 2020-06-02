splitbrain/dokuwiki-plugin-upgrade

commit 29c1ab7455581d75e9e58b69565c6132f4821db6

adjust version check for RCs. fixes #186

admin.php
@@ -241,7 +241,7 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
             $this->_warn($this->getLang('vs_tgzno').' '.hsc($http->error));
             $ok = false;
         }
-        if(!preg_match('/(^| )(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $tgzversion, $m)) {
+        if(!preg_match('/(^| )(?:rc)(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $tgzversion, $m)) {
             $this->_warn($this->getLang('vs_tgzno'));
             $ok            = false;
             $tgzversionnum = 0;
@@ -252,7 +252,7 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
 
         // get the current version
         $version = getVersion();
-        if(!preg_match('/(^| )(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $version, $m)) {
+        if(!preg_match('/(^| )(?:rc)(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $version, $m)) {
             $versionnum = 0;
         } else {
             $versionnum = $m[2];

commit d96773ca73cfa2849b12aaabba18ce7a439ca535

fix version recognition for non-RCs #186
This was broken in 29c1ab7455581d75e9e58b69565c6132f4821db6

admin.php
@@ -241,7 +241,7 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
             $this->_warn($this->getLang('vs_tgzno').' '.hsc($http->error));
             $ok = false;
         }
-        if(!preg_match('/(^| )(?:rc)(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $tgzversion, $m)) {
+        if(!preg_match('/(^| )(?:rc)?(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $tgzversion, $m)) {
             $this->_warn($this->getLang('vs_tgzno'));
             $ok            = false;
             $tgzversionnum = 0;
@@ -252,7 +252,7 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
 
         // get the current version
         $version = getVersion();
-        if(!preg_match('/(^| )(?:rc)(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $version, $m)) {
+        if(!preg_match('/(^| )(?:rc)?(\d\d\d\d-\d\d-\d\d[a-z]*)( |$)/i', $version, $m)) {
             $versionnum = 0;
         } else {
             $versionnum = $m[2];