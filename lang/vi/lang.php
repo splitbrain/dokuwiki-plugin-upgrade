8e4da928319d05eeadde1e91d47186512200703f
Thien Hau <thienhau.9a14@gmail.com>
2020-07-03 19:55:32 +0200

    translation update

--- lang/vi/lang.php
+++ lang/vi/lang.php
@@ -3,21 +3,21 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Thien Hau <thienhausoftware@gmail.com>
+ * @author Thien Hau <thienhau.9a14@gmail.com>
  */
-$lang['menu']                  = 'Năng cấp Wiki';
-$lang['vs_php']                = 'Bản phát hành DokuWiki mới cần ít nhất PHP %s, nhưng bạn đang chạy %s.  Bạn nên nâng cấp phiên bản PHP của mình trước khi nâng cấp!';
+$lang['menu']                  = 'Nâng cấp Wiki';
+$lang['vs_php']                = 'Bản phát hành DokuWiki mới cần tối thiểu PHP %s, nhưng bạn đang chạy %s. Bạn nên nâng cấp phiên bản PHP của mình trước khi nâng cấp!';
 $lang['vs_tgzno']              = 'Không thể xác định phiên bản mới nhất của DokuWiki.';
 $lang['vs_tgz']                = 'DokuWiki <b>%s</b> có sẵn để tải xuống';
 $lang['vs_local']              = 'Bạn hiện đang chạy DokuWiki <b>%s</b>.';
 $lang['vs_localno']            = 'Không rõ phiên bản hiện tại của bạn bao lâu, nên nâng cấp thủ công.';
-$lang['vs_newer']              = 'Có vẻ như DokuWiki hiện tại của bạn thậm chí còn mới hơn bản phát hành ổn định mới nhất.  Không khuyến nghị nâng cấp.';
+$lang['vs_newer']              = 'Có vẻ như DokuWiki hiện tại của bạn thậm chí còn mới hơn bản phát hành ổn định mới nhất. Không khuyến nghị nâng cấp.';
 $lang['vs_same']               = 'DokuWiki hiện tại của bạn đã được cập nhật. Không cần nâng cấp.';
 $lang['vs_plugin']             = 'Có một plugin có sẵn nâng cấp mới hơn (%s), bạn nên cập nhật plugin trước khi tiếp tục.';
 $lang['vs_ssl']                = 'PHP của bạn dường như không hỗ trợ các luồng SSL, việc tải xuống dữ liệu cần thiết rất có thể sẽ thất bại. Thay vào đó hãy nâng cấp thủ công.';
 $lang['dl_from']               = 'Đang tải xuống lưu trữ từ %s...';
 $lang['dl_fail']               = 'Tải về thất bại.';
-$lang['dl_done']               = 'Hoàn tất tải xuống  (%s).';
+$lang['dl_done']               = 'Hoàn tất tải xuống (%s).';
 $lang['pk_extract']            = 'Giải nén lưu trữ...';
 $lang['pk_fail']               = 'Giải nén thất bại.';
 $lang['pk_done']               = 'Giải nén xong.';
@@ -27,18 +27,18 @@ $lang['ck_done']               = 'Tất cả các tập tin đều có thể ghi
 $lang['ck_fail']               = 'Một số tập tin không thể ghi được. Không thể tự động nâng cấp.';
 $lang['cp_start']              = 'Đang cập nhật tập tin...';
 $lang['cp_done']               = 'Đã cập nhật tất cả tập tin.';
-$lang['cp_fail']               = 'Ùm. Đã xảy ra lỗi. Tốt hơn là nên kiểm tra thủ công.';
+$lang['cp_fail']               = 'Hùm. Đã xảy ra lỗi. Tốt hơn là nên kiểm tra thủ công.';
 $lang['tv_noperm']             = 'Không thể ghi được <code>%s</code>!';
-$lang['tv_upd']                = '<code>%s</code> sẽ được năng cấp.';
+$lang['tv_upd']                = '<code>%s</code> sẽ được nâng cấp.';
 $lang['tv_nocopy']             = 'Không thể xóa tập tin hiện có <code>%s</code> trước khi ghi đè!';
-$lang['tv_nodir']              = 'Khoing thể tạo đường dẫn <code>%s</code>!';
+$lang['tv_nodir']              = 'Không thể tạo đường dẫn <code>%s</code>!';
 $lang['tv_done']               = 'đã cập nhật <code>%s</code>';
 $lang['rm_done']               = 'Đã xóa <code>%s</code> lỗi thời.';
 $lang['rm_fail']               = 'Không thể xóa <code>%s</code> lỗi thời. <b>Vui lòng xóa thủ công!</b>';
 $lang['rm_mismatch']           = 'Trường hợp không phù hợp cho tập tin <code>%s</code> lỗi thời. Vui lòng kiểm tra thủ công nếu tập tin thực sự nên bị xóa.';
 $lang['finish']                = 'Nâng cấp hoàn thành. Thưởng thức DokuWiki mới của bạn';
 $lang['btn_continue']          = 'Tiếp tục';
-$lang['btn_abort']             = ' Hủy bỏ';
+$lang['btn_abort']             = 'Hủy bỏ';
 $lang['step_version']          = 'Kiểm tra';
 $lang['step_download']         = 'Tải về';
 $lang['step_unpack']           = 'Giải nén';
