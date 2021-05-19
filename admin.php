splitbrain/dokuwiki-plugin-upgrade

commit 27c6a3e69b508309747ba2073c9a3da14622c842

when dates are the same, but full version strings differ update anyway

admin.php
@@ -262,7 +262,7 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
             if($tgzversionnum < $versionnum) {
                 $this->_warn($this->getLang('vs_newer'));
                 $ok = false;
-            } elseif($tgzversionnum == $versionnum) {
+            } elseif($tgzversionnum == $versionnum && $tgzversion == $version) {
                 $this->_warn($this->getLang('vs_same'));
                 $ok = false;
             }

commit 6221d5c42e5fb4a52a9b7061fd50049b3ce2ba07

Adjust version number when we compare versions
Plugin alway shows an available upgrade. This is cause when we compare full version strings, they differ:
- $tgzversion value is: 2020-07-29 "Hogfather"
- $version value is: Release 2020-07-29 "Hogfather"

This fix use the getVersionData() function to ignore the "Release" string added using getVersion() function.

Also, $tgzversion value may contain spaces at the end. So we add a trim() for both variables.

Fix #201
The bug was introduced in commit 27c6a3e.

admin.php
@@ -236,7 +236,7 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
 
         // get the available version
         $http       = new DokuHTTPClient();
-        $tgzversion = $http->get($this->tgzversion);
+        $tgzversion = trim($http->get($this->tgzversion));
         if(!$tgzversion) {
             $this->_warn($this->getLang('vs_tgzno').' '.hsc($http->error));
             $ok = false;
@@ -250,7 +250,8 @@ class admin_plugin_upgrade extends DokuWiki_Admin_Plugin {
         }
 
         // get the current version
-        $version = getVersion();
+        $versiondata = getVersionData();
+        $version = trim($versiondata['date']);
         $versionnum = $this->dateFromVersion($version);
         self::_say($this->getLang('vs_local'), $version);
 