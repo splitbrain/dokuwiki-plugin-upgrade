870c630ad111892582ab0a5bdf095da7470e318d
Nurhayanti <luisuimi333@gmail.com>
2023-03-07 00:55:35 +0100

    translation update

--- lang/cy/lang.php
+++ lang/cy/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Alan Davies <ben.brynsadler@gmail.com>
  */
 $lang['menu']                  = 'Uwchraddiad Wici';
