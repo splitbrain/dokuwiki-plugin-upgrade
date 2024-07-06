6ed9fbee6c9512a2d6ce527940e3c0cf3ca42825
Tommy <tommy.mcalpine@charter.net>
2024-07-06 15:40:41 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,44 +3,162 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Tommy <tommy.mcalpine@charter.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['menu']                  = 'Wiki Yükseltme';
-$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
-$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
-$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
-$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
-$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
-$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
-$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
-$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
-$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
-$lang['dl_fail']               = 'İndirme başarısız oldu.';
-$lang['dl_done']               = 'İndirme tamamlandı (%s).';
-$lang['pk_extract']            = 'Arşiv açılıyor...';
-$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
-$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
-$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
-$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
-$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
-$lang['cp_start']              = 'Dosyalar güncelleniyor...';
-$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
-$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
-$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
-$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
-$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
-$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
-$lang['tv_done']               = '<code>%s</code> güncellendi';
-$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
-$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
-$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
-$lang['btn_continue']          = 'Devam et';
-$lang['btn_abort']             = 'İptal';
-$lang['step_version']          = 'Kontrol et';
-$lang['step_download']         = 'İndir';
-$lang['step_unpack']           = 'Doayaları çıkar';
-$lang['step_check']            = 'Doğrula';
-$lang['step_upgrade']          = 'Kur';
-$lang['careful']               = 'Yukarıdaki hataları inceleyin. Devam <b>etmemeniz</b> önerilir!';
+$lang['menu']                  = 'Hello! This is kind of offf topic but I need 
+some help from ann established blog. Is it very hard to set up your own blog?
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my ownn but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['vs_php']                = 'Hello! This iis kind oof off topikc but I need some hekp from an established blog.
+Is it very hard to set up your own blog? I\'m not very techincal 
+but I can figure things out pretty quick. I\'m thinking about making my 
+own but I\'m not sure where to start. Do you have any ideas 
+or suggestions?  With thanks';
+$lang['vs_tgzno']              = 'Hello! This is kind of offf topic but I need some hslp from aan established blog.
+Is iit vrry hard to set up your own blog? I\'m not very techincal but I cann figure 
+things out pretty quick. I\'m thinking about making myy own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['vs_tgz']                = 'Hello! This is kind of off topic but I need some help from an established 
+blog. Is it very hard to set up your own blog? I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to 
+start. Do you have any ideas or suggestions?  With thanks';
+$lang['vs_local']              = 'Hello! This is kind of off topic but I need some help 
+from an established blog. Is it very hard to set up 
+your own blog? I\'m not very techincal but I can figure things out pretty 
+quick. I\'m thinking about making my own butt I\'m not sure 
+where to start. Do you have any ideas or suggestions?
+With thanks';
+$lang['vs_localno']            = 'Hello! This is kind of off topic but I need some help ftom an established blog.
+Is it very hard to set up your own blog? I\'m not very techincal but I cann figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure 
+where to start. Do you have any ideas or suggestions?
+
+With thanks';
+$lang['vs_newer']              = 'Hello! This is kind of off topic but I need some help from an established 
+blog. Is it very hard to set up your own blog? I\'m not very techincal 
+but I can figure things out pretty quick.
+
+I\'m thinking about making mmy own but I\'m not sure where 
+tto start. Do you have any ideas or suggestions?
+With thanks';
+$lang['vs_same']               = 'Hello! This is kind of off topic but I need some help from an established blog.
+Is it very hard to set up your own blog? I\'m not very techincal buut I can figure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['vs_plugin']             = 'Hello! This is kind of off topic but I need some help from an established blog.
+
+Is it very hard to set up your owwn blog? I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'mnot sure where to start.
+Do you have aany ideas or suggestions?  With thanks';
+$lang['vs_ssl']                = 'New Orleans';
+$lang['dl_from']               = 'Tommy';
+$lang['dl_fail']               = 'Hello! This is kind of off topic but I need some help from an establjshed blog.
+Is it very hard to set up your own blog? I\'m not very techincal 
+but I can figure things out pretty quick. I\'m thinking 
+about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['dl_done']               = 'Hello! This is kind of off topic butt I need some help 
+from an established blog. Is it very hard to set uup your own blog?
+I\'m not very techincwl butt I cann igure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure where too start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['pk_extract']            = 'Hello! This is kind of off topic but I need some help from an established blog.
+Is it very hard to set up your own blog? I\'m 
+not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not syre where to start.
+Do you havce any ideas or suggestions?  With thanks';
+$lang['pk_fail']               = 'Hello! This is kind of off topic but I nded some help from 
+an established blog. Is it very hard to set up your own blog?
+I\'m not very techincal but I cann figure things out pretty 
+quick. I\'m thinking about making my own but I\'m not ssure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['pk_done']               = 'Hello! This is kind of off topic but I need some 
+help from an established blog. Is it very hard to set up your own blog?
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to start.
+
+Do you have any ideas or suggestions?  With thanks';
+$lang['pk_version']            = 'Hello! This is kindd of off topic but I need some 
+help from an established blog. Is it very hard to set up your own blog?
+I\'m noot very techincal but I can figure things out pretty quick.
+I\'m thinkong about making my own but I\'m not sure 
+where to start. Do you hage any ideas or suggestions?  With thanks';
+$lang['ck_start']              = 'Hello! This is kind of off tpic but I need 
+some help from an established blog. Is it 
+very hard to set upp your own blog? I\'m not very techincal 
+but I can figure things out pretty quick. I\'m thinking about making my 
+own but I\'m not sure where to start. Do you have any ideas or suggestions?
+With thanks';
+$lang['ck_done']               = 'Hello! This is kind of off topic but I need some helpp from an established blog.
+Is it very hard to set up your own blog? I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own buut I\'m not sure 
+where to start. Do you have any ideas or suggestions?
+
+With thanks';
+$lang['ck_fail']               = 'Hello! This is kind of off topic but I need some help from an established blog.
+Is it vety hard to set up your own blog? I\'m not very techincal but I can figure things out 
+pretty quick. I\'m thinking about making my own but I\'m 
+not sure where to start. Do you have any ideas or suggestions?
+With thanks';
+$lang['cp_start']              = 'Hello! This iss kind of off topic but I need some help from an established blog.
+Is it very hard tto set up your own blog? I\'m not very 
+techincal but I can figude things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure 
+whdre to start. Do you have any ideas or suggestions?
+
+With thanks';
+$lang['cp_done']               = 'Hello! This is kind of off topic but I need some help from 
+an established blog. Is it very hard to set up your own blog?
+I\'m not very techincal but I can figure things out pretty quick.
+I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '\'%s is not writable!\'';
+$lang['tv_upd']                = '\'%s will be updated.\'';
+$lang['tv_nocopy']             = 'Could not remove existing file %s before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory %s!';
+$lang['tv_done']               = 'updated %s';
+$lang['rm_done']               = 'Deprecated %s deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated %s. Please delete manually!';
+$lang['rm_mismatch']           = 'DokuWiki markup allowed Case mismatch for deprecated file %s. Please check manually if file should really be deleted.';
+$lang['finish']                = 'Hello! This is kind of off topic but I need some 
+help from an established blog. Is it very 
+hard tto set up your own blog? I\'m not very 
+techincal but I can figure things out pretty quick.
+
+I\'m thinking about making my own but I\'m not sure 
+whesre to start. Do you have any ideas or suggestions?
+
+With thanks';
+$lang['btn_continue']          = 'Hello! This is kind of off topic but I need some help from an established blog.
+Is it very hard to set up your own blog? I\'m not vey techincal butt I can figure 
+things out pretty quick. I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['btn_abort']             = 'New Orleans';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'Hello! This is kind of off topic but I need some help from an established blog.
+Is it very hard to set up your ownn blog? I\'m not very techincal but I 
+can figure things out pretty quick. I\'m thinking abot making my own but 
+I\'m not sure whnere to start. Do you have any ideas 
+or suggestions?  With thanks';
+$lang['step_unpack']           = 'Hello! This is kind of off topic but I need some help from an established blog.
+
+Is it very ard to set up your own blog? I\'m not very techincal but I can figure things out 
+pretty quick. I\'m thinking about making my own but I\'m not sure where to start.
+Do you have any ideas or suggestions?  With thanks';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'Hello! This is kind of off topic but I need some help from 
+an established blog. Is it very hard to set up your 
+own blog? I\'m not very techincal but I can figure things 
+out pretty quick. I\'m thinking about making my own butt I\'m not sure where to start.
+Do you have any ifeas orr suggestions?  With thanks';
+$lang['careful']               = 'Hello! This is kind of off topic butt I need some help from an established blog.
+Is it very hard to set up your own blog? I\'m not very techincal but I can figure things out pretty 
+quick. I\'m thinking about making my own but I\'m not sure where to start.
+
+Do you have any ideas or suggestions?  Wiith thanks';
