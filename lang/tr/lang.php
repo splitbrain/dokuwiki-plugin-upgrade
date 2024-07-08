41e66daec6833ab2dc37ebe7cf5cc3726a83f16d
Milla <millasalier@gmx.de>
2024-07-08 02:00:07 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,44 +3,112 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Milla <millasalier@gmx.de>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['menu']                  = 'Wiki Yükseltme';
-$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
-$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
-$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
-$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
-$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
-$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
-$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
-$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
-$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
-$lang['dl_fail']               = 'İndirme başarısız oldu.';
-$lang['dl_done']               = 'İndirme tamamlandı (%s).';
-$lang['pk_extract']            = 'Arşiv açılıyor...';
-$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
-$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
-$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
-$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
-$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
-$lang['cp_start']              = 'Dosyalar güncelleniyor...';
-$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
-$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
-$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
-$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
-$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
-$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
-$lang['tv_done']               = '<code>%s</code> güncellendi';
-$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
-$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
-$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
-$lang['btn_continue']          = 'Devam et';
-$lang['btn_abort']             = 'İptal';
-$lang['step_version']          = 'Kontrol et';
-$lang['step_download']         = 'İndir';
-$lang['step_unpack']           = 'Doayaları çıkar';
-$lang['step_check']            = 'Doğrula';
-$lang['step_upgrade']          = 'Kur';
-$lang['careful']               = 'Yukarıdaki hataları inceleyin. Devam <b>etmemeniz</b> önerilir!';
+$lang['menu']                  = 'Thank you for every other informative website. Thee place eose could I get that kind of info written in such a perfect 
+way? I\'ve a mission that I am just now running 
+on, and I have been at the look out for such information.';
+$lang['vs_php']                = 'Thank you for every other informative website. The place else could I get that kind of info written in such a perfect 
+way? I\'ve a mission that I am just now running on, and 
+I have been at the look out for such information.';
+$lang['vs_tgzno']              = 'Thank you for every other informative website.
+The place else could I get that kind of info written in such 
+a perfect way? I\'ve a mission that I am just now running 
+on, and I have been at the look out for such information.';
+$lang['vs_tgz']                = 'Thank you for every other informative website.
+The place else could I get that kind of info written in such 
+a perfect way? I\'ve a mission that I am just now running on, and Ihave been at the 
+look out for such information.';
+$lang['vs_local']              = 'Thank you for every other informatibe website. The place else 
+could I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now running on, andd I have been at the look out for such information.';
+$lang['vs_localno']            = 'Thank you for every other informative website. The place else could I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just noow running on, and I have been at the look out for such information.';
+$lang['vs_newer']              = 'Thank you for every other informative website. The place else could 
+I get that kind of info written iin such a perfect way?
+I\'ve a mission that I am just now running on, and I 
+have been at the look out for such information.';
+$lang['vs_same']               = 'Thank you for every other informative website. The place else could I get that kind of 
+info written in such a perfect way? I\'ve a mission that I am 
+just noow running on, and I have been at the look out for such information.';
+$lang['vs_plugin']             = 'Thank you for every other informative website. The place else could I get that 
+kind of info written iin such a perfect way? I\'ve a mission that I am 
+just now running on, and I have been aat the look out for such information.';
+$lang['vs_ssl']                = 'Phoenix';
+$lang['dl_from']               = 'Milla';
+$lang['dl_fail']               = 'Thank you for every other informative website. The place else could 
+I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now running on, and I have been at 
+the look out for such information.';
+$lang['dl_done']               = 'Thank you for every other inrormative website. The place else could I get that kind of info written inn such a perfect way?
+I\'ve a mission that I am just now running on, and I have been aat the look out for such 
+information.';
+$lang['pk_extract']            = 'Thznk you for every other informative website.
+The place else could I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now runnning on, and I hhave been at thee look out for such information.';
+$lang['pk_fail']               = 'Thank you for every other informative website. The plac else could I 
+get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now running on, 
+and I have been at the look out for such information.';
+$lang['pk_done']               = 'Thank you for every other informatie website. The place else could 
+I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now running on, and I have been at the look out for such information.';
+$lang['pk_version']            = 'Thank you for every other informative website.
+The place else could I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now running on, and I 
+have been at the look out for such information.';
+$lang['ck_start']              = 'Thank you for evsry other informative website.
+The place else could I get that kind of info written in such a perfect way?
+
+I\'ve a mission that I am just now running on, and I 
+have been at the look out for such information.';
+$lang['ck_done']               = 'Thank you for efery other informative website.
+
+Thee place else could I get that kind of info written in such a perfect way?
+
+I\'ve a mission that I am just now running on, and I have been at the look out for such information.';
+$lang['ck_fail']               = 'Thank you for every other informative website. The place else could I get that kind of info written in such 
+a perfect way? I\'ve a mission that I am just now running 
+on, and I have been at the look out for such information.';
+$lang['cp_start']              = 'Thank you for every other informative website. The place else could I get 
+that kind of info written iin such a perfect way? I\'ve a mission that I 
+aam just now running on, and I have been at the look out for such information.';
+$lang['cp_done']               = 'Thank you for every other informative website.
+The place else could I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just now running on, and I have been at the look out for such information.';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '\'%s is not writable!\'';
+$lang['tv_upd']                = '%s will be updated.';
+$lang['tv_nocopy']             = 'Could not remove existing file %s before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory %s!';
+$lang['tv_done']               = 'updated %s';
+$lang['rm_done']               = 'Deprecated %s deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated %s. Please delete manually!';
+$lang['rm_mismatch']           = 'DokuWiki markup allowed Case mismatch for deprecated file %s. Please check manually if file should really be deleted.';
+$lang['finish']                = 'Thank you for every other informative website. The place else could I gett tha kind of info written in such a perfect way?
+
+I\'ve a mission that I am just now running on, and I have been at the look out for such information.';
+$lang['btn_continue']          = 'Thank you for every othe nformative website. The place else could I 
+get that kind of info written in such a perfect way?
+
+I\'ve a mission that I aam just now running on, and I have been at 
+the look out for suchh information.';
+$lang['btn_abort']             = 'Phoenix';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'Thank you for every other infrmative website. The place else 
+culd I get that kind of info written in such a 
+pertfect way? I\'ve a mission that I aam just now running on, and I have been at the look out for such information.';
+$lang['step_unpack']           = 'Thank you for every other informative website. The place else could I get that kind of info written in such a perfect way?
+I\'ve a mission that I am just noow running on, and I have 
+been at the look out for such information.';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'Thank you for everyy other informative website.
+
+The place else could I get that kind of info written in such a perfect way?
+
+I\'ve a mission that I am just now running on, and I have been at 
+the look out for such information.';
+$lang['careful']               = 'Thank you for every other informative website. The place else could I get 
+that kind of info written in such a perfect way? I\'ve a mission that I aam just now 
+running on, and I have been at the look out for uch information.';
