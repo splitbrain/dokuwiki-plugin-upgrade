9cd2847dd665129e1c2624a18233858f8e275f0d
Hassan <hassanzepps@aol.com>
2024-08-11 21:20:06 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,44 +3,165 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hassan <hassanzepps@aol.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['menu']                  = 'Wiki Yükseltme';
-$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
-$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
-$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
-$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
-$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
-$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
-$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
-$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
-$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
-$lang['dl_fail']               = 'İndirme başarısız oldu.';
-$lang['dl_done']               = 'İndirme tamamlandı (%s).';
-$lang['pk_extract']            = 'Arşiv açılıyor...';
-$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
-$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
-$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
-$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
-$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
-$lang['cp_start']              = 'Dosyalar güncelleniyor...';
-$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
-$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
-$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
-$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
-$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
-$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
-$lang['tv_done']               = '<code>%s</code> güncellendi';
-$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
-$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
-$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
-$lang['btn_continue']          = 'Devam et';
-$lang['btn_abort']             = 'İptal';
-$lang['step_version']          = 'Kontrol et';
-$lang['step_download']         = 'İndir';
-$lang['step_unpack']           = 'Doayaları çıkar';
-$lang['step_check']            = 'Doğrula';
-$lang['step_upgrade']          = 'Kur';
-$lang['careful']               = 'Yukarıdaki hataları inceleyin. Devam <b>etmemeniz</b> önerilir!';
+$lang['menu']                  = 'An impressive share! I have just forwarded this ontoo a colleague who has been doing a little homework on this.
+And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time to talk about 
+this subject here on your intrernet site.';
+$lang['vs_php']                = 'An impressive share! I have just forwarded this onto a colleague who 
+has been doing a little homework on this. And he actuaally 
+bought me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks forr the meal!!
+But yeah, thanx for spending som time to talk about this subject here on your internet site.';
+$lang['vs_tgzno']              = 'An impressive share! I have just forwarded this onto a colleague who has 
+been doing a little homework on this. And he actually bought me dinner simply because 
+I stumbled upon it for him... lol. So allow me to reword this....
+Thanks for the meal!! But yeah, thanx for spending some 
+time to talk about this subject here on your internet site.';
+$lang['vs_tgz']                = 'An impressive share! I have just forwarded thijs onto a colleague who has been dling 
+a little homework on this. And he actually bought 
+me dinner simply because I stumbled upon it for 
+him... lol. So allow me to reword this.... Thanks for 
+the meal!! But yeah, thanx for spending some time to talk about this subject here on your internet site.';
+$lang['vs_local']              = 'An impressive share! I have just forwarded this 
+onto a colleague who has been doing a little homework on this.
+
+And he actually bought me dinner simply because I stumbled upon it for him...
+
+lol. So allkow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time to talk about this subject here on your internet 
+site.';
+$lang['vs_localno']            = 'An impressive share! I have just fowarded this onto a 
+colleague who has been doing a little homework on this. And hee actually bought me 
+dinner simply because I stumbled upon it for him... lol. So allow me to reword this....
+
+Thanks for the meal!! But yeah, thanx for spending some time to talk about this subject here 
+on your internet site.';
+$lang['vs_newer']              = 'An impressive share! I have just forwarded this onto a colleague who has been doing a little homework on this.
+And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So alloow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time to talk 
+about this subject here on your internet site.';
+$lang['vs_same']               = 'An impressive share! I have just forwarded this onto a colledague who 
+has been doing a little homework on this. And he actually 
+bbought me dinner simply because I stumbled upon it for him...
+
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time too talk aboiut this subject here 
+on your internet site.';
+$lang['vs_plugin']             = 'An impressive share! I have just forwarded this onto a colleague who has been doing a little homework on this.
+And he actually bought me dinner simply because I stumbled 
+upon it for him... lol. So allow me to reword this....
+
+Thanks for the meal!! But yeah, thanx for spending some time to talk about this subject here on your internet site.';
+$lang['vs_ssl']                = 'Leofara';
+$lang['dl_from']               = 'Hassan';
+$lang['dl_fail']               = 'An impressive share! I have just forwarded this onto a colleague who has been doing a 
+little homewkrk on this. And he actually bought me 
+dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time to talk ablut 
+this subject here on your internet site.';
+$lang['dl_done']               = 'An impressive share! I have just forwarded this onto a colleague who has been doing a little homework on this.
+
+And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So allow mme to reword this.... Thanks for the meal!!
+
+But yeah, thanx for spending some time to talk about this subject here on yiur internet site.';
+$lang['pk_extract']            = 'An impressive share! I have just forwarded this onto a colleague who has 
+been doing a little homework on this. And he actually bought me 
+dinner simply because I stumbled upon it foor him...
+
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some tme to talk about 
+this subject here on your internet site.';
+$lang['pk_fail']               = 'An impressive share! I have just forwarded this onto a colleague 
+who has been doing a little homework onn this.
+
+And he actually bought me dinner simply because I stumbled upon it for him...
+
+lol. So allow mme to reword this.... Thanks for the meal!!
+
+But yeah, thanx for spendng some time to talk about this subject here 
+on your internet site.';
+$lang['pk_done']               = 'An impressive share! I have just forwarded this onto a colleague who has been doing a 
+little homework on this. And hee actually bought me dinner simply because 
+I stumbled upon it for him... lol. So allow me tto reword this....
+
+Thanks for the meal!! Butt yeah, thanx for spending some time to talk about this 
+subject here on your internet site.';
+$lang['pk_version']            = 'An impressive share! I have just forwarded this onto a 
+colleague who has been doikng a little homework on this. And hhe actually bought 
+me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx foor spending some time to talk about this subject here on your internet site.';
+$lang['ck_start']              = 'An impressive share! I have just forwarded this onto a colleague who has 
+been doing a little homework on this. And he actually bought me dinner simply 
+because I stumbled upon it for him... lol. So allow me to reword this....
+
+Thanks for the meal!! But yeah, thanx for spending some time to 
+talk about this subject here on youir internet site.';
+$lang['ck_done']               = 'An impressive share! I have just forwarded thiss onto a colleague who has been doing a little 
+homework on this. And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks ffor the meal!!
+
+But yeah, thanx for spending some time to talk about this subject here on your internet site.';
+$lang['ck_fail']               = 'An impressive share! I have just forwarded thhis onto a colleague who has been doing a little homework 
+onn this. And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!! But yeah, thanx for spending some time to talk 
+about this subject here on your internet site.';
+$lang['cp_start']              = 'An impressive share! Ihave just fforwarded this onto 
+a colleague who has been doing a little homework 
+on this. And he actually bought mee dinner simply because I 
+stumbled upon it for him... lol. So allow me to reword this....
+Thanks for thhe meal!! But yeah, than foor spending some time to talk about this subject 
+here on your internet site.';
+$lang['cp_done']               = 'An impressive share! I have just forwardeed this onto a colleague who has been doing 
+a little homework on this. And he actually bought me dinner simply because I 
+stumbled upon it for him... lol. So allow me to reword this....
+Thanks for the meal!! Buut yeah, thanx for spending some timne to talk about this 
+subject here on your internet site.';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '%s is not writable!';
+$lang['tv_upd']                = '%s will be updated.';
+$lang['tv_nocopy']             = 'Could not remove existing file %s before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory %s!';
+$lang['tv_done']               = 'updated %s';
+$lang['rm_done']               = 'Deprecated %s deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated %s. Please delete manually!';
+$lang['rm_mismatch']           = 'DokuWiki markup allowed Case mismatch for deprecated file %s. Please check manually if file should really be deleted.';
+$lang['finish']                = 'An impressive share! I have just forwarded this onto a colleague who has been doing a little homework on this.
+And he actually bought mme dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!But yeah, thanx for spending some time to talk 
+about this subject here on your inernet site.';
+$lang['btn_continue']          = 'An impressive share! I have just forwarded this onto a colleague who has 
+been doing a little homework on this. And he actually bought me dinner simply because 
+I stumbled upon it for him... lol. So allow me to rewor this....
+Thanks for the meal!! But yeah, thanx for spending some time to talk about this subject here on your internet site.';
+$lang['btn_abort']             = 'Leofara';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'An impressive share! I have just forwarded this onto a 
+colleague who has been doing a little homework on this. And he actually bought me dinner simply because Istumbled upon it 
+for him... lol. So allow mme tto reword this.... Thanks for the meal!!
+But yeah, thganx for spending some time to talk about this subject here onn your internet 
+site.';
+$lang['step_unpack']           = 'An impressive share! I have just forwarded this onto a colleague who has been doing a little homeaork on this.
+
+And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time to talk about this 
+subject here onn yur internet site.';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'An impressive share! I have just forwarded this onto a colleague 
+who has been doing a little homework on this. And he actually bought mme dinner 
+simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some timje to talk about this subject here on your internet site.';
+$lang['careful']               = 'An impressive share! I have just forwarded thiss onto a colleague who has been doing a 
+little homework on this. And he actually bought me dinner simply because I stumbled upon it for him...
+lol. So allow me to reword this.... Thanks for the meal!!
+But yeah, thanx for spending some time to 
+talk about this subject here on your internet site.';
