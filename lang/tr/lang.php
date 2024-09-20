c8fffba23a25e4ba2e3ebff076d31f099e2adcf0
Alfie <alfiepederson@comcast.net>
2024-09-20 14:05:46 +0200

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,44 +3,152 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Alfie <alfiepederson@comcast.net>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['menu']                  = 'Wiki Yükseltme';
-$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
-$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
-$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
-$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
-$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
-$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
-$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
-$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
-$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
-$lang['dl_fail']               = 'İndirme başarısız oldu.';
-$lang['dl_done']               = 'İndirme tamamlandı (%s).';
-$lang['pk_extract']            = 'Arşiv açılıyor...';
-$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
-$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
-$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
-$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
-$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
-$lang['cp_start']              = 'Dosyalar güncelleniyor...';
-$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
-$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
-$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
-$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
-$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
-$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
-$lang['tv_done']               = '<code>%s</code> güncellendi';
-$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
-$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
-$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
-$lang['btn_continue']          = 'Devam et';
-$lang['btn_abort']             = 'İptal';
-$lang['step_version']          = 'Kontrol et';
-$lang['step_download']         = 'İndir';
-$lang['step_unpack']           = 'Doayaları çıkar';
-$lang['step_check']            = 'Doğrula';
-$lang['step_upgrade']          = 'Kur';
-$lang['careful']               = 'Yukarıdaki hataları inceleyin. Devam <b>etmemeniz</b> önerilir!';
+$lang['menu']                  = 'Hi, I ddo believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine however,
+if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up!
+Besides that,wonderful website!';
+$lang['vs_php']                = 'Hi, I do believe your website could possibly be having internet browser compatibility 
+issues. When I take a loook at your weeb ssite in Safari, it looks fine however, 
+if opening in IE, it\'s got some overlapping issues.
+
+I simply wanted to give you a quick heads up! Besides that,
+wonderful website!';
+$lang['vs_tgzno']              = 'Hi, I do believe your webssite could possbly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks 
+fine however, if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up! Besides that, 
+wonderful website!';
+$lang['vs_tgz']                = 'Hi, I do believe your website could possibly be having internet browser 
+compatibility issues. When I take a look at your web site in Safari, it looks finje however, if opening in IE, it\'s got some 
+overlapping issues. I simply wanted to give you a quick 
+heads up! Besides that, wonderful website!';
+$lang['vs_local']              = 'Hi, I do believe your website could possibly 
+be having internet browsser compatibility issues. When I take 
+a look at your web site in Safari, it looks fine however,
+if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give yoou a quick heads up! Besides that, wonderful website!';
+$lang['vs_localno']            = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine however,
+if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up!
+
+Besides that, wonderful website!';
+$lang['vs_newer']              = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine 
+however, if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give yyou a quick heads up!
+Besides that, wonderful website!';
+$lang['vs_same']               = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine 
+however, iif opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up! Besides that, wonderful website!';
+$lang['vs_plugin']             = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, 
+it looks fine however, if opening inn IE, it\'s got 
+some overlapping issues. I simplyy wanted to give you a quick heads up!
+Besides that, wonderful website!';
+$lang['vs_ssl']                = 'Murray';
+$lang['dl_from']               = 'Alfie';
+$lang['dl_fail']               = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine however,
+iif opening in IE, it\'s got some overlapping issues.
+I simply wanted to give yyou a quick heads up! Besides that, wonderful website!';
+$lang['dl_done']               = 'Hi, I do beleve your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine however, if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up!
+Besides that, wonderful website!';
+$lang['pk_extract']            = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+Wheen I take a look at youhr web site in Safari, it looks fine however, 
+if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up! Besides that, wonderful website!';
+$lang['pk_fail']               = 'Hi, I do believee your website could possibly be having internet browser 
+compatibility issues. When I take a look at your web site in Safari, it looks fine however,if opening in IE, it\'s gott some overlapping issues.
+I simply wanted to give you a quick heads up! Besides 
+that, wonderful website!';
+$lang['pk_done']               = 'Hi, I do believe your website could possibly be having 
+injternet browser compatibility issues. When I take 
+a look at your web site in Safari, it looks fine 
+however, if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up! Besides that,
+wonderful website!';
+$lang['pk_version']            = 'Hi,I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine however, 
+if opening in IE, it\'s got some overlapping 
+issues. I simply wanted to give you a quick heads up!
+Besides that, wonderful website!';
+$lang['ck_start']              = 'Hi, I ddo believe your website coould possibly be having internet browser compatibility issues.
+
+When I take a look aat your web site in Safari, 
+it looks fine however, if opening in IE, it\'s got 
+some overlapping issues. I simply wanted to give you a quick heads up!
+
+Besides that, wonderful website!';
+$lang['ck_done']               = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks 
+fine however, if opening in IE, it\'s got some overlappiong 
+issues. I simply wanted to give you a quick heads up!
+Besides that, wonderful website!';
+$lang['ck_fail']               = 'Hi, I do believe your website could possibly be 
+having internet browser compatibility issues.
+When I take a look at your web site in Safari, it 
+looks fine however, if opening in IE, it\'s got somke 
+overlapping issues. I simply wanted to give you a quick heads up!
+Besides that, wonderful website!';
+$lang['cp_start']              = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it 
+lookks fine however, if opening in IE, it\'s got some 
+overlapping issues. I simply wanted to give you a quick heads 
+up! Besides that, wonderful website!';
+$lang['cp_done']               = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine however,
+if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up! Besides that, wonderful website!';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '`%s is not writable!`';
+$lang['tv_upd']                = '%s will be updated.';
+$lang['tv_nocopy']             = 'Could not remove existing file %s before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory %s!';
+$lang['tv_done']               = 'updated %s';
+$lang['rm_done']               = 'Deprecated %s deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated %s. Please delete manually!';
+$lang['rm_mismatch']           = 'DokuWiki markup allowed Case mismatch for deprecated file %s. Please check manually if file should really be deleted.';
+$lang['finish']                = 'Hi, I do believe your website could possibly be having 
+internet browser compatibility issues. When I take a look at your web site in Safari, 
+it looks fine however, iff opening in IE, it\'s got some overlapping issues.
+I simply wanted to give you a quick heads up! Besides that, wonderful website!';
+$lang['btn_continue']          = 'Hi, I do believe your website could possibly be having internet browser compatibiolity issues.
+
+When I take a look at your web site in Safari, it looks 
+fine however, if opening in IE, it\'s got some overlapping 
+issues. Isimply wanted to give you a quick heads 
+up! Besides that, wonderful website!';
+$lang['btn_abort']             = 'Murray';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'Hi, I do believe your website could possibly be having internet browser compatibility 
+issues. When I take a look at your web site in Safari, it looks fine however,
+if opening in IE, it\'s got some overlapping issues.
+I simply wanted to give youu a quick heads up!
+Besides that, wonderful website!';
+$lang['step_unpack']           = 'Hi, I do believe your website could possibly be having internet browser compatibijlity issues.
+When I take a look at your web site in Safari, it looks fine 
+however, if opening in IE, it\'s got some overlapping issues.
+
+I simply wanted to give you a quick heads up!
+
+Besides that, wonderful website!';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'Hi, I do believe your website could possibly be having internet browser compatibility issues.
+When I take a look at your web site in Safari, it looks fine 
+however, if opening in IE, it\'s got some overlapping 
+issues. I simply wanted to give you a quick heads up!
+Besides that, wonderful website!';
+$lang['careful']               = 'Hi, I do beloeve your website could possibly 
+be havijng internet browser compatibility issues. When I 
+taske a look at your wweb site in Safari,
+it looks fine however, if opening in IE, it\'s got some overlapping issues.
+
+I simply wanted to give you a quick heads up! Besides that, wonderful website!';
