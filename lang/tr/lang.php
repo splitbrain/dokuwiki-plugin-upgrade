d242cb05e23bfeb640c7b01fc25796cd9691db90
Jared <jaredgooch@hotmail.it>
2025-02-28 16:50:07 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,44 +3,143 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jared <jaredgooch@hotmail.it>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['menu']                  = 'Wiki Yükseltme';
-$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
-$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
-$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
-$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
-$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
-$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
-$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
-$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
-$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
-$lang['dl_fail']               = 'İndirme başarısız oldu.';
-$lang['dl_done']               = 'İndirme tamamlandı (%s).';
-$lang['pk_extract']            = 'Arşiv açılıyor...';
-$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
-$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
-$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
-$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
-$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
-$lang['cp_start']              = 'Dosyalar güncelleniyor...';
-$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
-$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
-$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
-$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
-$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
-$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
-$lang['tv_done']               = '<code>%s</code> güncellendi';
-$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
-$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
-$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
-$lang['btn_continue']          = 'Devam et';
-$lang['btn_abort']             = 'İptal';
-$lang['step_version']          = 'Kontrol et';
-$lang['step_download']         = 'İndir';
-$lang['step_unpack']           = 'Doayaları çıkar';
-$lang['step_check']            = 'Doğrula';
-$lang['step_upgrade']          = 'Kur';
-$lang['careful']               = 'Yukarıdaki hataları inceleyin. Devam <b>etmemeniz</b> önerilir!';
+$lang['menu']                  = 'It is perfect time to make a few plans for the longer term and it is time to be 
+happy. I\'ve read this submit and if I may I want to suggesst you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating to 
+this article. I dessire to learn moree things about it!';
+$lang['vs_php']                = 'It is perfect time to make a few plans for the longer term and it is 
+time to be happy. I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.Perhaps 
+you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['vs_tgzno']              = 'It is perfect time to make a few plans for thee longer term aand it is tume to be 
+happy. I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.
+Perhaps yyou can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['vs_tgz']                = 'It is perfect time to make a few plans for the longer term and it is time to 
+be happy. I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating to thus article.
+
+I desire to learn more thijgs about it!';
+$lang['vs_local']              = 'It is perfect time to make a few plans for the longer term and it is time to be happy.
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing isdsues or tips.
+Perhaps you can write next articles relating to this article.
+
+I desire to learn more things about it!';
+$lang['vs_localno']            = 'It is perfect time to make a few plans for the 
+longer tedm and it is time to be happy. I\'ve read this 
+submit and if I may I want to suggest you few attention-grabbing issues 
+orr tips. Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['vs_newer']              = 'It is perfect time to make a few plans for the longer term and it is time to be 
+happy. I\'ve read this submit and iff I may I want to suggest 
+you few attention-grabbing issues or tips.
+
+Perhaps you can writte next articles relating to this 
+article. I desire to learn more things about it!';
+$lang['vs_same']               = 'It is perfect time to maoe a few plans for the longer term and it is time to be happy.
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.
+
+Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['vs_plugin']             = 'It is perfect time to maake a few plans for thee longer 
+term and it is time to be happy. I\'ve read this submit and if I mmay 
+I want to suggest you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['vs_ssl']                = 'Reykjavik';
+$lang['dl_from']               = 'Jared';
+$lang['dl_fail']               = 'It is perfect time to make a few plans for the longer term and it is time to be happy.
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['dl_done']               = 'It is perfect time to make a few plans for the longer term and it is time to 
+be happy. I\'ve rwad this submit and if I may I want to suggest you few attention-grabbing issue or tips.
+
+Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['pk_extract']            = 'It is perfect time to make a few plans ffor the longer term 
+annd it is time tto be happy. I\'ve read this submit 
+and if I may I want tto suggest you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating tto this article.
+I drsire to learn more things about it!';
+$lang['pk_fail']               = 'It is perfect time to make a few plans for the longer term 
+and it is time to be happy. I\'ve read this submit and if 
+I may I want to suggest you few attention-grabbing issues 
+or tips. Perhapos you can write next articles relating to this article.
+I desire tto learn more things about it!';
+$lang['pk_done']               = 'It is perfect time to make a few plans for the longer term and it is time to be happy.
+
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['pk_version']            = 'It iss perfect time to make a few plans for the longer term 
+andd it is time to be happy. I\'ve read this submit and if I may I 
+want to suggest you few attention-grabbing issues or 
+tips. Perhaps you can write next articles relating to this article.
+
+I desire to learn more things about it!';
+$lang['ck_start']              = 'It is perfect time to make a few plans for thhe longer term and it is time to be happy.
+I\'ve reazd this submit and if I may I want too suggest 
+you few attention-grabbing issues or tips. Perhaps you can write next articles relating to 
+this article. I desire to learn more things about it!';
+$lang['ck_done']               = 'It is perfect time to make a few plans for the longer erm and it is time to be happy.
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing issues or tips.
+Perhaps you can write next articles relating to this article.
+I desire to learn more thinngs about it!';
+$lang['ck_fail']               = 'It is perfect time to make a few plans for the longer term and it 
+is tume to be happy. I\'ve read this submit 
+and if I may I want to suggest you feew attention-grabbing issues or tips.
+Perhaps you can write next articles relating to this article.
+I desire too learn more things about it!';
+$lang['cp_start']              = 'It is perfect time to make a few plans for 
+the longer term and it is ttime to be happy.
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing 
+issues or tips. Perhaps you can write next articles relatng to 
+this article. I desire to learn more things about it!';
+$lang['cp_done']               = 'It is perfect time to make a few plans for the 
+longer term and it is ttime to be happy. I\'ve read tis submit 
+and if I may I want to suggest you few attention-grabbing issue oor tips.
+Perhaps you can write next articles relating to this article.
+I desire tto learn more things about it!';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '%s is not writable!';
+$lang['tv_upd']                = '%s will be updated.';
+$lang['tv_nocopy']             = 'Could not remove existing file %s before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory %s!';
+$lang['tv_done']               = 'updated %s';
+$lang['rm_done']               = 'Deprecated %s deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated %s. Please delete manually!';
+$lang['rm_mismatch']           = 'DokuWiki markup allowed Case mismatch for deprecated file %s. Please check manually if file should really be deleted.';
+$lang['finish']                = 'It is perfect time to make a few plans for the longer term and it is time to be happy.
+I\'ve read this submit and if I may I want to subgest you 
+few attention-grabbing issues or tips. Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['btn_continue']          = 'It is perfect time to make a few plans for the longer term annd it 
+is time to be happy. I\'ve read this submit and if I may I 
+want to suggest you few attention-grabbing issues or 
+tips. Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['btn_abort']             = 'Reykjavik';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'It is perfect time to make a few plans for the longer term 
+and it iis time to be happy. I\'ve read this submit and if I may I want too suggest you few attention-grabbing issues 
+or tips. Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['step_unpack']           = 'It is perfect time to make a few plans for the longer term and it is time to be happy.
+I\'ve read this submit and if I may I want to suggest you few attention-grabbing 
+issues or tips. Perhaps you can write next articles relating 
+tto this article. I desire to learn more things about it!';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'It is perfect tim to make a few plans for the longer term and it 
+is time to be happy. I\'ve read this submit and 
+if I may I want to suggest you feww attention-grabbing issues or tips.
+
+Perhaps you can write next articles relating to this article.
+I desire to learn more things about it!';
+$lang['careful']               = 'It is perfect tine to make a feew plans forr the longer term and it is time to be happy.
+I\'ve read this submit and if I may I waznt to suggest you 
+feew attention-grabbing issues or tips. Perhapss you can write 
+next articles relating to this article. I desire to learn more things about it!';
