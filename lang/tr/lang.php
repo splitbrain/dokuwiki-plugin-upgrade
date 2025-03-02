0469b94ff951e4cb948a2cb796c451a3f88936ab
Tesha <tesharied@gmail.com>
2025-03-02 15:15:46 +0100

    translation update

--- lang/tr/lang.php
+++ lang/tr/lang.php
@@ -3,44 +3,106 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Tesha <tesharied@gmail.com>
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['menu']                  = 'Wiki Yükseltme';
-$lang['vs_php']                = 'Yeni DokuWiki sürümü en azından PHP %s gerektirir, ancak siz %s kullanıyorsunuz. Devam etmeden önce PHP sürümünüzü yükseltmelisiniz.';
-$lang['vs_tgzno']              = 'DokuWiki\'nin en yeni sürümü tespit edilemedi.';
-$lang['vs_tgz']                = 'DokuWiki <b>%s</b> indirilmek için hazır.';
-$lang['vs_local']              = 'Kullanmakta olduğunuz DokuWiki <b>%s</b>.';
-$lang['vs_localno']            = 'Kullandığınız sürümün tarihi tespit edilemedi. Elle yükseltme yapmanız tavsiye edilir.';
-$lang['vs_newer']              = 'Görünüşe göre en güncel kararlı sürümden daha yeni bir DokuWiki kullanıyorsunuz. Yükseltme yapmanız önerilmez.';
-$lang['vs_same']               = 'DokuWiki\'niz güncel. Yükseltmeye gerek yok.';
-$lang['vs_plugin']             = 'Yükseltme eklentisinin daha güncel bir sürümü mevcut (%s) Devam etmeden önce eklentiyi güncellemelisiniz.';
-$lang['vs_ssl']                = 'Görünüşe göre PHP oturumunuz SSL yayınlarını desteklemiyor. Gerekli dosyaların indirilmesi yüksek ihtimalle başarısız olacak. Elle güncelleme yapmanız önerilir.';
-$lang['dl_from']               = 'Arşiv, %s adresinden indiriliyor...';
-$lang['dl_fail']               = 'İndirme başarısız oldu.';
-$lang['dl_done']               = 'İndirme tamamlandı (%s).';
-$lang['pk_extract']            = 'Arşiv açılıyor...';
-$lang['pk_fail']               = 'Arşivin açılması başarısız oldu.';
-$lang['pk_done']               = 'Arşivin açıklası tamamlandı.';
-$lang['pk_version']            = 'DokuWiki <b>%s</b> , kurulmaya hazır (Şu an kullanmakta olduğunuz sürüm <b>%s</b>).';
-$lang['ck_start']              = 'Dosya izinleri kontrol ediliyor...';
-$lang['ck_done']               = 'Tüm dosyalar yazılabilir. Yükseltme için hazır.';
-$lang['ck_fail']               = 'Bazı dosyalar yazılabilir değil. Otomatik yükseltme mümkün değil.';
-$lang['cp_start']              = 'Dosyalar güncelleniyor...';
-$lang['cp_done']               = 'Tüm dosyalar güncellendi.';
-$lang['cp_fail']               = 'Offf! Birşey yanlış gitti. En iyisi elle kontrol edin.';
-$lang['tv_noperm']             = '<code>%s</code> yazılabilir değil!';
-$lang['tv_upd']                = '<code>%s</code> güncellenecek.';
-$lang['tv_nocopy']             = '<code>%s</code> dosyası kopyalanamıyor!';
-$lang['tv_nodir']              = '<code>%s</code> klasörü oluşturulamıyor!';
-$lang['tv_done']               = '<code>%s</code> güncellendi';
-$lang['rm_done']               = 'Çakışan <code>%s</code> silindi.';
-$lang['rm_fail']               = 'Çakışan <code>%s</code> silinemiyor. <b>Lütfen elle silin!</b>';
-$lang['finish']                = 'Yükseltme tamamlandı. Yeni DokuWiki\'nizin keyfini çıkarın';
-$lang['btn_continue']          = 'Devam et';
-$lang['btn_abort']             = 'İptal';
-$lang['step_version']          = 'Kontrol et';
-$lang['step_download']         = 'İndir';
-$lang['step_unpack']           = 'Doayaları çıkar';
-$lang['step_check']            = 'Doğrula';
-$lang['step_upgrade']          = 'Kur';
-$lang['careful']               = 'Yukarıdaki hataları inceleyin. Devam <b>etmemeniz</b> önerilir!';
+$lang['menu']                  = 'Pretty section of content. I just stumbled upon your skte and in accession capital to assert that I 
+get actually enjoyed account yor blog posts. Anyway I will be subscribing to your feeds and 
+even I achievement you access consistently fast.';
+$lang['vs_php']                = 'Pretty section of content. Ijust stumbled upon your site and in accession capital to 
+assert that I get actually enjoyed account your blog posts.
+Anyway I will be subscribing to your feeds and even I achievement you access 
+consistently fast.';
+$lang['vs_tgzno']              = 'Pretty sectikon of content. I just stumbled upon your site and in accession capital to 
+assert that I get actually enjoyed account your blog posts.
+
+Anyway I will be subscribing to your feeds and even I achievement 
+you access consistently fast.';
+$lang['vs_tgz']                = 'Pretty section of content. I just stumbled upon your site and iin accession capital to assert that 
+I get actually enjoyed account your blog posts. Anyway I will be subscribing to your feeds and even I achievement you 
+access consistently fast.';
+$lang['vs_local']              = 'Pretty section of content. I just stumbled uon your 
+site and in accession capital to assert that 
+I get actually enjoyed account your blog posts. Anyway I will bee subscribing to 
+yohr feeds and even I achievement you access consisrently fast.';
+$lang['vs_localno']            = 'Pretty section of content. I just stumbled upon your 
+site and in accession capital to asssert that I get actually enjoyed account your blog posts.
+Anyway I will be subscribing to your feeds and even I achievement you access consisteently fast.';
+$lang['vs_newer']              = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert that I get actually enjoyed accounnt your 
+blog posts. Anyway I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['vs_same']               = 'Pretty section of content. I just stumbled upon your site and in accession capial to assert that I get actually enjoyed account your blog posts.
+Anyway I will be subscribing to youjr feeds and even I 
+achievement you access consistently fast.';
+$lang['vs_plugin']             = 'Pretty section of content. I just stumbled upon your 
+site and in accession capital to assert that I get actually enjoyed account your blig posts.
+Anyway I will be subscribing to your feeds and even I achievement 
+you access consistently fast.';
+$lang['vs_ssl']                = 'Paris';
+$lang['dl_from']               = 'Tesha';
+$lang['dl_fail']               = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert that I get actually enjoyed account your blog posts.
+Anyway I wiill be subscribing to your feeds andd even I achievement you access consistently fast.';
+$lang['dl_done']               = 'Pretty section of content. I just stumbled upon your site 
+and in accessio capital to assert that I geet actualy enjoyed account 
+your blog posts. Anyway I will be subscribing to your feeds and 
+even I achievement you access consistently fast.';
+$lang['pk_extract']            = 'Pretty section of content. I just stumbled upon your site annd in accession capital to assert that 
+I get actually enjoyed account your blog posts. Anyway I will be subscribing 
+to your feeds and even I achievement you access 
+consistently fast.';
+$lang['pk_fail']               = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert that I get actually 
+enjoyed account your blog posts. Anyway I will be subscribing 
+to your feeds and even I achievement you access consistently fast.';
+$lang['pk_done']               = 'Pretty section of content. I just stumbled upon your site and in accession capital to 
+assaert that I get actually enjoyed account your blog posts.
+Anyway I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['pk_version']            = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert that I get actually 
+enjoyed account your blog posts. Anyway I will be subscribing to your 
+feeds and even I achievement you access consistently fast.';
+$lang['ck_start']              = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert thaat I get actually enjoyed account 
+yoiur blog posts. Anyway I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['ck_done']               = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert that I get actually enjoyed account your blog 
+posts. Anyway I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['ck_fail']               = 'Pretty section of content. I just stumbled upon your site 
+and in accession capital to assert that I get actually enjoyted accont your blog posts.
+Anyway I will be subscribing to your feeds and even I achievement you 
+access consistently fast.';
+$lang['cp_start']              = 'Pretty section of content. I just stumbled upon your site aand in accession capital 
+to assert that I get actually enjoyed account your blog posts.
+Anyway I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['cp_done']               = 'Pretty section of content. I just stumbled upon your 
+site and in accession capital to assert that I get actually 
+enjoyed account you blog posts. Anyway I will be subscribing to your feeds and even I 
+achievement you access consistently fast.';
+$lang['cp_fail']               = 'Uh-Oh. Something went wrong. Better check manually.';
+$lang['tv_noperm']             = '%s is not writable!';
+$lang['tv_upd']                = '%s will be updated.';
+$lang['tv_nocopy']             = 'Could not remove existing file %s before overwriting!';
+$lang['tv_nodir']              = 'Could not create directory %s!';
+$lang['tv_done']               = 'updated %s';
+$lang['rm_done']               = 'Deprecated %s deleted.';
+$lang['rm_fail']               = 'Could not delete deprecated %s. Please delete manually!';
+$lang['rm_mismatch']           = 'DokuWiki markup allowed Case mismatch for deprecated file %s. Please check manually if file should really be deleted.';
+$lang['finish']                = 'Pretty section of content. I just stumbled uoon your site and in accession capital to assert that I get actually enjoyed account your 
+blog posts. Anyway I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['btn_continue']          = 'Pretty section of content. I just stumbled 
+upon your site and in accession capital to assert that I get 
+actually enjoyed account your blog posts.
+
+Anyway I will be subscribing to your feeds aand even I achievement you access consistently fast.';
+$lang['btn_abort']             = 'Paris';
+$lang['step_version']          = 'Check';
+$lang['step_download']         = 'Pretty section of content. I just stumbled 
+upon your sitge and in accession capital to assert that I get actually enjoyed account your blog posts.
+
+Anywy I will be subscribing to your feeds and even I achievement you access consistently fast.';
+$lang['step_unpack']           = 'Pretty section of content. I just stumbled upon your site and in accession capital to 
+assert that I get actually enjoyed account your blog posts.
+Anyway I will be subscdibing to your feeds and even I 
+achievement you access consistently fast.';
+$lang['step_check']            = 'Verify';
+$lang['step_upgrade']          = 'Pretty section of content. I just stumbled upon your site and in accession capital to assert that I get actually enjoyed account your blog posts.
+Anyway I will bbe subscriing to yoiur feeds and even I achievement you 
+access consistently fast.';
+$lang['careful']               = 'Pretty section of content. I just stumbled upin your site and in accession capital to 
+assert that I get actually enjoyed account your blog posts.
+Antway I will be subscribing to your feeds and even I achievement yyou access consistently fast.';
